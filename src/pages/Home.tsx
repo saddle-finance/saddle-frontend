8f75d7f5f90f39fb10e120ea21800d314a4cfe7f
sandrasong <sandra.song@consensys.net>
2020-09-16 16:46:18 -0400

    Setup react-router-dom with Home and Swap pages (#48)
    * Added home&pool path and react router
    
    * Add top navigation
    
    * Top menu UI
    
    * remove unused import and switch to functional component for Home
    
    Co-authored-by: Sunil Srivatsa <sunilsrivatsa@gmail.com>

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+function Home() {
+  return (
+    <main className="homepage">
+      <h1>This is swap page</h1>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
+
+import App from "./App"
+import Home from "./pages/Home"
+import Pool from "./pages/Pool"
+
+export const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/home" component={Home} />
+      <Route path="/pool" component={Pool} />
+    </Switch>
+  </Router>
+)
