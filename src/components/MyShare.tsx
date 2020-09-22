792b95400e2a85fa008c1995e8f4dadbd1e034e5
sandrasong <chromsketch@gmail.com>
2020-09-22 16:08:49 -0400

    Style and add selective display of my share component

--- src/components/MyShare.tsx
+++ src/components/MyShare.tsx
@@ -1,32 +1,39 @@
 import React from "react"
 import "./MyShare.scss"
 interface Props {
-  data: any
+  data?: {
+    name: string
+    share: number
+    USDbalance: number
+    token: string[]
+  }
 }
 
 function MyShare({ data }: Props) {
-  return (
-    <div className="myShare">
-      <h4>My Share</h4>
-      <div className="table">
-        <div className="info">
-          <div className="poolShare">
-            <span>{data.name}</span>
-            <span>{data.share}</span>
+  if (!data) return null
+  else
+    return (
+      <div className="myShare">
+        <h4>My Share</h4>
+        <div className="table">
+          <div className="info">
+            <div className="poolShare">
+              <span className="label name">{data.name}</span>
+              <span>{data.share}</span>
+            </div>
+            <div className="balance">
+              <span className="label">USD Balance</span>
+              <span>{data.USDbalance}</span>
+            </div>
+            <div className="currency">
+              <span className="label">Currency</span>
+              <span>{data.token.map((coin) => coin + ", ")}</span>
+            </div>
           </div>
-          <div className="balance">
-            <span>USD Balance</span>
-            <span>{data.USDbalance}</span>
-          </div>
-          <div className="currency">
-            <span>Currency</span>
-            <span>{data.token}</span>
-          </div>
+          <button className="withdraw">Withdraw</button>
         </div>
-        <button className="withdraw">Withdraw</button>
       </div>
-    </div>
-  )
+    )
 }
 
 export default MyShare
