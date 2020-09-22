9895d0962a42b15f8138fd6fca786bab264686c9
sandrasong <chromsketch@gmail.com>
2020-09-22 14:45:44 -0400

    Style pool overview component

--- src/components/PoolOverview.tsx
+++ src/components/PoolOverview.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import classNames from "classnames"
 
 import "./PoolOverview.scss"
 
@@ -17,30 +18,38 @@ function PoolOverview({ data }: Props) {
     <div className="poolOverview">
       <h4>Pool Overview</h4>
       <div className="table">
-        <span>{data.title}</span>
+        <div className="left">
+          <span className="title">{data.title}</span>
+          <span style={{ marginRight: "8px" }}>[</span>
+          {data.tokens.map((token, index) => (
+            <div className="token" key={index}>
+              <img alt="" src={token.icon} />
+              <span>{token.name}</span>
+            </div>
+          ))}
+          <span style={{ marginLeft: "-8px" }}>]</span>
+        </div>
 
-        <span>[</span>
-        {data.tokens.map((token, index) => (
-          <div key={index}>
-            <img alt="" src={token.icon} />
-            <span>{token.name}</span>
+        <div className="right">
+          <div className="Apy">
+            <span className="label">APY</span>
+            <span
+              className={
+                classNames({ plus: data.APY }) +
+                classNames({ minus: !data.APY })
+              }
+            >
+              {data.APY ? "+" + data.APY : "-" + data.APY}
+            </span>
           </div>
-        ))}
-        {/* <img alt="" src={data.token[0].icon}></img>
-    <span>{data.token[0].name}</span> */}
-        <span>]</span>
-
-        <div className="Apy">
-          <span>APY</span>
-          <span>{data.APY}</span>
-        </div>
-        <div className="saddApy">
-          <span>SADD APY</span>
-          <span>{data.saddAPY}</span>
-        </div>
-        <div className="volume">
-          <span>Volume</span>
-          <span>{data.volume}</span>
+          <div className="saddApy">
+            <span className="label">SADD APY</span>
+            <span>{data.saddAPY}</span>
+          </div>
+          <div className="volume">
+            <span className="label">Volume</span>
+            <span>${data.volume}</span>
+          </div>
         </div>
       </div>
     </div>
