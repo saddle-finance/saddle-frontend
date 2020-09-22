6fc3fcdab7f222c57cbbe3d7971c1406c40af5bd
sandrasong <chromsketch@gmail.com>
2020-09-21 23:26:10 -0400

    Create pool page structure

---
+++ src/pages/PoolBTC.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+function PoolBTC() {
+  return (
+    <div>
+      <h1>Pool BTC page</h1>
+    </div>
+  )
+}
+
+export default PoolBTC
---
+++ src/pages/PoolUSD.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+function PoolUSD() {
+  return (
+    <div>
+      <h1>Pool USD page</h1>
+    </div>
+  )
+}
+
+export default PoolUSD
