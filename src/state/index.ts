e31b065be9f34bc099c0c287eeb04688e0314d1a
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2020-09-29 10:35:49 -0700

    Add redux-localstorage-simple to save/load the Redux store (#72)
    Closes #10

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,12 +1,19 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+import { save, load } from "redux-localstorage-simple"
 
 import user from "./user"
+
+const PERSISTED_KEYS: string[] = ["user"]
 
 const store = configureStore({
   reducer: {
     user,
   },
-  middleware: [...getDefaultMiddleware({ thunk: false })],
+  middleware: [
+    ...getDefaultMiddleware({ thunk: false }),
+    save({ states: PERSISTED_KEYS }),
+  ],
+  preloadedState: load({ states: PERSISTED_KEYS }),
 })
 
 export default store
