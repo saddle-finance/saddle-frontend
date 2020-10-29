8d4fa9bbd7abcce87ba098e220725d846dbdf97c
sandrasong <chromsketch@gmail.com>
2020-10-29 15:57:51 -0400

    Add pool explanation on pool page

--- src/pages/Pool.tsx
+++ src/pages/Pool.tsx
@@ -24,6 +24,20 @@ function Pool(): ReactElement {
             <AssetButton title="BTC" to="/pool/btc" icon={btcIcon} />
           </div>
         </div>
+        <p>
+          Liquidity pools, in essence, are pools of tokens that are locked in a
+          smart contract. They are used to facilitate trading by providing
+          liquidity and are extensively used by some of the decentralized
+          exchanges a.k.a DEXes.
+          <br />
+          <br />
+          When liquidity is supplied to a pool, the liquidity provider (LP)
+          receives special tokens called LP tokens in proportion to how much
+          liquidity they supplied to the pool. When a trade is facilitated by
+          the pool a 0.3% feis proportionally distributed amongst all the LP
+          token holders. If the liquidity provider wants to get their underlying
+          liquidity back, plus any accrued fees, they must burn their LP tokens.
+        </p>
       </div>
     </div>
   )
