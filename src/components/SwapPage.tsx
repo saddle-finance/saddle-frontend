4c2787ed166a3fc0dd56387f707768e6ac24f667
sandrasong <chromsketch@gmail.com>
2020-10-29 00:04:50 -0400

    Update info section, action button and error on swap page

--- src/components/SwapPage.tsx
+++ src/components/SwapPage.tsx
@@ -69,30 +69,31 @@ const SwapPage = (props: Props): ReactElement => {
           tokens={tokens}
           selected={selectedTokens[1]}
         />
-        <div className="priceTable">
-          <span className="title">{t("price")}</span>
-          <span className="pair">{rate.pair}</span>
-          <button className="exchange">
-            <svg
-              width="24"
-              height="20"
-              viewBox="0 0 24 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17.4011 12.4196C17.4011 13.7551 16.5999 13.8505 16.4472 13.8505H6.62679L9.14986 11.3274L8.47736 10.6501L5.13869 13.9888C5.04986 14.0782 5 14.1991 5 14.3251C5 14.4511 5.04986 14.572 5.13869 14.6613L8.47736 18L9.14986 17.3275L6.62679 14.8044H16.4472C17.1054 14.8044 18.355 14.3274 18.355 12.4196V10.9888H17.4011V12.4196Z"
-                fill="#D67A0A"
-              />
-              <path
-                d="M5.9539 7.58511C5.9539 6.24965 6.75519 6.15426 6.90781 6.15426H16.7283L14.2052 8.67733L14.8777 9.34984L18.2164 6.01117C18.3052 5.92181 18.355 5.80092 18.355 5.67492C18.355 5.54891 18.3052 5.42803 18.2164 5.33867L14.8777 2L14.2004 2.67727L16.7283 5.20035H6.90781C6.24962 5.20035 5 5.6773 5 7.58511V9.01597H5.9539V7.58511Z"
-                fill="#D67A0A"
-              />
-            </svg>
-          </button>
-          <span className="value">{rate.value}</span>
-        </div>
-        <div className="advancedOptions">
+        <div className="infoSection">
+          <div className="priceTable">
+            <span className="title">{t("price")}</span>
+            <span className="pair">{rate.pair}</span>
+            <button className="exchange">
+              <svg
+                width="24"
+                height="20"
+                viewBox="0 0 24 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.4011 12.4196C17.4011 13.7551 16.5999 13.8505 16.4472 13.8505H6.62679L9.14986 11.3274L8.47736 10.6501L5.13869 13.9888C5.04986 14.0782 5 14.1991 5 14.3251C5 14.4511 5.04986 14.572 5.13869 14.6613L8.47736 18L9.14986 17.3275L6.62679 14.8044H16.4472C17.1054 14.8044 18.355 14.3274 18.355 12.4196V10.9888H17.4011V12.4196Z"
+                  fill="#3800D6"
+                />
+                <path
+                  d="M5.9539 7.58511C5.9539 6.24965 6.75519 6.15426 6.90781 6.15426H16.7283L14.2052 8.67733L14.8777 9.34984L18.2164 6.01117C18.3052 5.92181 18.355 5.80092 18.355 5.67492C18.355 5.54891 18.3052 5.42803 18.2164 5.33867L14.8777 2L14.2004 2.67727L16.7283 5.20035H6.90781C6.24962 5.20035 5 5.6773 5 7.58511V9.01597H5.9539V7.58511Z"
+                  fill="#3800D6"
+                />
+              </svg>
+            </button>
+            <span className="value">{rate.value}</span>
+          </div>
+          <div className="cost">{info.isInfo ? info.message : "..."}</div>
           <div
             className="title"
             onClick={(): PayloadAction<boolean> =>
@@ -113,14 +114,13 @@ const SwapPage = (props: Props): ReactElement => {
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M14.8252 0C16.077 0 16.3783 0.827943 15.487 1.86207L8.80565 9.61494C8.35999 10.1321 7.63098 10.1246 7.19174 9.61494L0.510262 1.86207C-0.376016 0.833678 -0.0777447 0 1.17205 0L14.8252 0Z"
-                fill="#EA860B"
+                fill="#00f4d7"
               />
             </svg>
           </div>
-
-          {/* When advanced = true, divider will be shown */}
-          <div className={"divider " + classNames({ show: advanced })}></div>
-
+        </div>
+        <div className="advancedOptions">
+          <div className="divider"></div>
           {/* When advanced = true, this table will be shown */}
           <div className={"tableContainer " + classNames({ show: advanced })}>
             <div className="table">
@@ -235,9 +235,6 @@ const SwapPage = (props: Props): ReactElement => {
             </div>
           </div>
         </div>
-        <div className={"error " + classNames({ showError: error.isError })}>
-          {error.message}
-        </div>
         <button
           className={"swap " + classNames({ disabled: error.isError })}
           onClick={(): void => {
@@ -248,8 +245,8 @@ const SwapPage = (props: Props): ReactElement => {
         >
           {t("swap")}
         </button>
-        <div className={"info " + classNames({ showInfo: info.isInfo })}>
-          {info.message}
+        <div className={"error " + classNames({ showError: error.isError })}>
+          {error.message}
         </div>
         <Modal isOpen={modalOpen} onClose={(): void => setModalOpen(false)}>
           {popUp === "review" ? (
