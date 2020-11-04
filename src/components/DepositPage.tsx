1134812ff2ec9b3b3d1225c92d543fce18ed5fa5
sandrasong <chromsketch@gmail.com>
2020-11-04 01:11:21 -0500

    Enhance checkbox to be outlined

--- src/components/DepositPage.tsx
+++ src/components/DepositPage.tsx
@@ -167,11 +167,30 @@ const DepositPage = (props: Props): ReactElement => {
             <div className="divider"></div>
             <div className={"tableContainer" + classNames({ show: advanced })}>
               <div className="infiniteApproval">
-                <input
-                  type="checkbox"
-                  checked={infiniteApproval}
-                  onChange={(): void => setInfiniteApproval(!infiniteApproval)}
-                />
+                <label className="checkbox_input">
+                  <input
+                    type="checkbox"
+                    checked={infiniteApproval}
+                    onChange={(): void =>
+                      setInfiniteApproval(!infiniteApproval)
+                    }
+                  />
+                  <span className="checkbox_control">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                      focusable="false"
+                    >
+                      <path
+                        fill="none"
+                        stroke="#00f4d7"
+                        strokeWidth="4"
+                        d="M1.73 12.91l6.37 6.37L22.79 4.59"
+                      />
+                    </svg>
+                  </span>
+                </label>
                 <span>{t("infiniteApproval")}</span>
               </div>
               <div className="paramater">
