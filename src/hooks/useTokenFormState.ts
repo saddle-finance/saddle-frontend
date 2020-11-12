69dedd991aff67c40f05ead98dbac667d0a21c24
David Hamme <dhamme@gmail.com>
2020-11-11 21:41:42 -0500

    Hook up slippage to redux. Overhaul representation of numbers. (#173)

---
+++ src/hooks/useTokenFormState.ts
@@ -0,0 +1,52 @@
+import {
+  NumberInputState,
+  numberInputStateCreator,
+} from "../utils/numberInputState"
+
+import { BigNumber } from "@ethersproject/bignumber"
+import React from "react"
+import { Token } from "../constants"
+
+interface TokensStateType {
+  [key: string]: NumberInputState
+}
+type UpdateTokenStateFnType = (tokenSymbol: string, value: string) => void
+type UseTokenFormStateReturnType = [TokensStateType, UpdateTokenStateFnType]
+
+export function useTokenFormState(
+  tokens: Token[],
+): UseTokenFormStateReturnType {
+  // Token input state handlers
+  const tokenInputStateCreators: {
+    [tokenSymbol: string]: ReturnType<typeof numberInputStateCreator>
+  } = tokens.reduce(
+    (acc, token) => ({
+      ...acc,
+      [token.symbol]: numberInputStateCreator(
+        token.decimals,
+        BigNumber.from("0"),
+      ),
+    }),
+    {},
+  )
+
+  // Token input values, both "raw" and formatted "safe" BigNumbers
+  const [tokenFormState, setTokenFormState] = React.useState<TokensStateType>(
+    tokens.reduce(
+      (acc, token) => ({
+        ...acc,
+        [token.symbol]: tokenInputStateCreators[token.symbol]("0"),
+      }),
+      {},
+    ),
+  )
+
+  function updateTokenFormValue(tokenSymbol: string, value: string): void {
+    setTokenFormState((prevState) => ({
+      ...prevState,
+      [tokenSymbol]: tokenInputStateCreators[tokenSymbol](value),
+    }))
+  }
+
+  return [tokenFormState, updateTokenFormValue]
+}
