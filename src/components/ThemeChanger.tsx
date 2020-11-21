cdac439b045a6393901d3dc78f05a1f6e388216f
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2020-11-20 21:19:28 -0800

    Store the user dark mode setting in Redux state

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,31 +1,32 @@
 import "./ThemeChanger.scss"
 
-import React, { ReactElement, useEffect, useState } from "react"
+import { AppDispatch, AppState } from "../state"
+import React, { ReactElement, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import { PayloadAction } from "@reduxjs/toolkit"
+import { updateDarkMode } from "../state/user"
 
 const ThemeChanger = (): ReactElement => {
-  const [themeState, setThemeState] = useState(false)
+  const dispatch = useDispatch<AppDispatch>()
+  const { userDarkMode } = useSelector((state: AppState) => state.user)
 
   useEffect(() => {
-    const getTheme = localStorage.getItem("Theme")
-    if (getTheme === "dark") {
-      setThemeState(true)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (themeState) {
-      localStorage.setItem("Theme", "dark")
+    if (userDarkMode) {
       document.body.classList.add("dark")
     } else {
-      localStorage.setItem("Theme", "light")
       document.body.classList.remove("dark")
     }
-  }, [themeState])
+  }, [userDarkMode])
 
   return (
     <div className="themeChanger">
-      <button onClick={(): void => setThemeState(!themeState)}>
-        {themeState ? "☾" : "☀"}
+      <button
+        onClick={(): PayloadAction<boolean> =>
+          dispatch(updateDarkMode(!userDarkMode))
+        }
+      >
+        {userDarkMode ? "☾" : "☀"}
       </button>
     </div>
   )
