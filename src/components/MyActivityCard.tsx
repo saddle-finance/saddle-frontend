11af1f022be0ef2a0af6cfc4e117a7a7413bda6b
sandrasong <chromsketch@gmail.com>
2021-01-19 16:01:07 -0500

    Seperate and style activity data in MyShare panel

---
+++ src/components/MyActivityCard.tsx
@@ -0,0 +1,75 @@
+import "./MyActivityCard.scss"
+
+import React, { ReactElement } from "react"
+import { HistoricalPoolDataType } from "../hooks/useHistoricalPoolData"
+import { formatUnits } from "@ethersproject/units"
+import { useTranslation } from "react-i18next"
+
+interface Props {
+  historicalPoolData: HistoricalPoolDataType | null
+}
+
+function MyActivityCard({ historicalPoolData }: Props): ReactElement | null {
+  const { t } = useTranslation()
+
+  if (!historicalPoolData) return null
+
+  const historicalFormattedData = historicalPoolData
+    ? {
+        totalDepositsBTC: parseFloat(
+          formatUnits(historicalPoolData.totalDepositsBTC, 36),
+        ).toFixed(5),
+        totalWithdrawalsBTC: parseFloat(
+          formatUnits(historicalPoolData.totalWithdrawalsBTC, 36),
+        ).toFixed(5),
+        totalDepositsUSD: parseFloat(
+          formatUnits(historicalPoolData.totalDepositsUSD, 36),
+        ).toFixed(),
+        totalWithdrawalsUSD: parseFloat(
+          formatUnits(historicalPoolData.totalWithdrawalsUSD, 36),
+        ).toFixed(),
+        totalProfitBTC: parseFloat(
+          formatUnits(historicalPoolData.totalProfitBTC, 36),
+        ).toFixed(5),
+        totalProfitUSD: parseFloat(
+          formatUnits(historicalPoolData.totalProfitUSD, 36),
+        ).toFixed(),
+      }
+    : null
+
+  return (
+    <div className="myActivityCard">
+      <h4>{t("myActivity")}</h4>
+      {historicalFormattedData ? (
+        <div className="activityTable">
+          <div key="deposits-btc">
+            <span className="label">BTC {t("deposit")}: </span>
+            <span>{historicalFormattedData.totalDepositsBTC}</span>
+          </div>
+          <div key="deposits-usd">
+            <span className="label">USD {t("deposit")}: </span>
+            <span>{historicalFormattedData.totalDepositsUSD}</span>
+          </div>
+          <div key="withdrawals-btc">
+            <span className="label">BTC {t("withdrawal")}: </span>
+            <span>{historicalFormattedData.totalWithdrawalsBTC}</span>
+          </div>
+          <div key="withdrawals-usd">
+            <span className="label">USD {t("withdrawal")}: </span>
+            <span>{historicalFormattedData.totalWithdrawalsUSD}</span>
+          </div>
+          <div key="profit-btc">
+            <span className="label">BTC {t("profit")}: </span>
+            <span>{historicalFormattedData.totalProfitBTC}</span>
+          </div>
+          <div key="profit-usd">
+            <span className="label">USD {t("profit")}: </span>
+            <span>{historicalFormattedData.totalProfitUSD}</span>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default MyActivityCard
--- src/components/MyShareCard.tsx
+++ src/components/MyShareCard.tsx
@@ -3,17 +3,15 @@ import "./MyShareCard.scss"
 import React, { ReactElement } from "react"
 import { commify, formatUnits } from "@ethersproject/units"
 
-import { HistoricalPoolDataType } from "../hooks/useHistoricalPoolData"
 import { TOKENS_MAP } from "../constants"
 import { UserShareType } from "../hooks/usePoolData"
 import { useTranslation } from "react-i18next"
 
 interface Props {
   data: UserShareType | null
-  historicalPoolData: HistoricalPoolDataType | null
 }
 
-function MyShareCard({ data, historicalPoolData }: Props): ReactElement | null {
+function MyShareCard({ data }: Props): ReactElement | null {
   const { t } = useTranslation()
 
   if (!data) return null
@@ -33,29 +31,6 @@ function MyShareCard({ data, historicalPoolData }: Props): ReactElement | null {
       }
     }),
   }
-
-  const historicalFormattedData = historicalPoolData
-    ? {
-        totalDepositsBTC: parseFloat(
-          formatUnits(historicalPoolData.totalDepositsBTC, 36),
-        ).toFixed(5),
-        totalWithdrawalsBTC: parseFloat(
-          formatUnits(historicalPoolData.totalWithdrawalsBTC, 36),
-        ).toFixed(5),
-        totalDepositsUSD: parseFloat(
-          formatUnits(historicalPoolData.totalDepositsUSD, 36),
-        ).toFixed(),
-        totalWithdrawalsUSD: parseFloat(
-          formatUnits(historicalPoolData.totalWithdrawalsUSD, 36),
-        ).toFixed(),
-        totalProfitBTC: parseFloat(
-          formatUnits(historicalPoolData.totalProfitBTC, 36),
-        ).toFixed(5),
-        totalProfitUSD: parseFloat(
-          formatUnits(historicalPoolData.totalProfitUSD, 36),
-        ).toFixed(),
-      }
-    : null
 
   return (
     <div className="myShareCard">
@@ -85,34 +60,6 @@ function MyShareCard({ data, historicalPoolData }: Props): ReactElement | null {
           </div>
         ))}
       </div>
-      {historicalFormattedData ? (
-        <div className="historicalPoolData">
-          <div key="deposits-btc">
-            <span className="label">{t("totalDeposits")} (BTC): </span>
-            <span>{historicalFormattedData.totalDepositsBTC}</span>
-          </div>
-          <div key="withdrawals-btc">
-            <span className="label">{t("totalWithdrawals")} (BTC): </span>
-            <span>{historicalFormattedData.totalWithdrawalsBTC}</span>
-          </div>
-          <div key="profit-btc">
-            <span className="label">{t("totalProfit")} (BTC): </span>
-            <span>{historicalFormattedData.totalProfitBTC}</span>
-          </div>
-          <div key="deposits-usd">
-            <span className="label">{t("totalDeposits")} (USD): </span>
-            <span>{historicalFormattedData.totalDepositsUSD}</span>
-          </div>
-          <div key="withdrawals-usd">
-            <span className="label">{t("totalWithdrawals")} (USD): </span>
-            <span>{historicalFormattedData.totalWithdrawalsUSD}</span>
-          </div>
-          <div key="profit-usd">
-            <span className="label">{t("totalProfit")} (USD): </span>
-            <span>{historicalFormattedData.totalProfitUSD}</span>
-          </div>
-        </div>
-      ) : null}
     </div>
   )
 }
