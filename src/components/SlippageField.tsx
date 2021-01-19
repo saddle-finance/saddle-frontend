b67db7d0048f55e3fe51439fff0ff14b63df7eaa
sandrasong <chromsketch@gmail.com>
2021-01-19 18:48:09 -0500

    Adjust space in GasField and SlippageField

--- src/components/SlippageField.tsx
+++ src/components/SlippageField.tsx
@@ -53,7 +53,7 @@ export default function SlippageField(): ReactElement {
             dispatch(updateSlippageCustom(e.target.value))
           }
         />
-        %
+        &nbsp;%
       </div>
     </div>
   )
