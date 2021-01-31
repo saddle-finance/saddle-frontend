3331e8b40454b0bae29a7e46c58c861cd7b24645
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2021-01-30 21:34:50 -0800

    Setup web vitals metrics reporting
    Closes #278

--- src/utils/googleAnalytics.ts
+++ src/utils/googleAnalytics.ts
@@ -1,8 +1,28 @@
+import { Metric } from "web-vitals"
+
 export function logEvent(name: string, params: Record<string, string>): void {
   // gtag is only loaded in index.html when NODE_ENV is set to production, so check if it's a function
   if (typeof window.gtag === "function") {
     window.gtag("event", name, params)
   } else {
-    console.debug(`Logging event '${name}' with parameters: `, params)
+    console.debug(`Logging event ${name} with parameters:`, params)
+  }
+}
+
+// See: https://github.com/GoogleChrome/web-vitals#using-gtagjs-google-analytics-4
+export function sendWebVitalsToGA({ name, delta, value, id }: Metric): void {
+  if (typeof window.gtag === "function") {
+    window.gtag("event", name, {
+      // Use the metric delta as the event's value parameter.
+      value: delta,
+      // Everything below is a custom event parameter.
+      web_vitals_metric_id: id, // Needed to aggregate events.
+      web_vitals_metric_value: value, // Optional
+      // Any additional params or metadata (e.g. debug information) can be
+      // set here as well, within the following limitations:
+      // https://support.google.com/analytics/answer/9267744
+    })
+  } else {
+    console.debug(`Reporting web vitals metric ${name}:`, delta, value, id)
   }
 }
