39ba05ba2466303b4f16a54caabec52ef9326831
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2021-02-03 17:30:31 -0800

    Log errors to Google Analytics (#306)
    * Log errors to Google Analytics
    
    Closes #284
    
    * Pass logError to addEventListener directly

--- src/utils/googleAnalytics.ts
+++ src/utils/googleAnalytics.ts
@@ -1,6 +1,6 @@
 import { Metric } from "web-vitals"
 
-export function logEvent(name: string, params: Record<string, string>): void {
+export function logEvent(name: string, params: Record<string, unknown>): void {
   // gtag is only loaded in index.html when NODE_ENV is set to production, so check if it's a function
   if (typeof window.gtag === "function") {
     window.gtag("event", name, params)
@@ -9,20 +9,23 @@ export function logEvent(name: string, params: Record<string, string>): void {
   }
 }
 
+export function logError(error: ErrorEvent): void {
+  logEvent("exception", {
+    description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
+    fatal: true,
+  })
+}
+
 // See: https://github.com/GoogleChrome/web-vitals#using-gtagjs-google-analytics-4
 export function sendWebVitalsToGA({ name, delta, value, id }: Metric): void {
-  if (typeof window.gtag === "function") {
-    window.gtag("event", name, {
-      // Use the metric delta as the event's value parameter.
-      value: delta,
-      // Everything below is a custom event parameter.
-      web_vitals_metric_id: id, // Needed to aggregate events.
-      web_vitals_metric_value: value, // Optional
-      // Any additional params or metadata (e.g. debug information) can be
-      // set here as well, within the following limitations:
-      // https://support.google.com/analytics/answer/9267744
-    })
-  } else {
-    console.debug(`Reporting web vitals metric ${name}:`, delta, value, id)
-  }
+  logEvent(name, {
+    // Use the metric delta as the event's value parameter.
+    value: delta,
+    // Everything below is a custom event parameter.
+    web_vitals_metric_id: id, // Needed to aggregate events.
+    web_vitals_metric_value: value, // Optional
+    // Any additional params or metadata (e.g. debug information) can be
+    // set here as well, within the following limitations:
+    // https://support.google.com/analytics/answer/9267744
+  })
 }
