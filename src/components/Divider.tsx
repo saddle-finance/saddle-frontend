3f4171706abbe7da0a50ae684b3a148bfb58ccf7
David Hamme <dhamme@gmail.com>
2021-03-08 22:01:34 -0800

    brand new files. Two complex components, a hook, and scss

---
+++ src/components/Divider.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react"
+
+import styles from "./Divider.module.scss"
+
+const Divider = (): ReactElement => <div className={styles.divider}></div>
+
+export default Divider
