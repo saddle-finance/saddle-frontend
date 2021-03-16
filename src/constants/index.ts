d8a467d969e7e055579458e0a4a42b4d75a614eb
Jongseung Lim <penandlim@gmail.com>
2021-03-16 16:05:40 -0400

    Update contract addresses

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -56,7 +56,7 @@ export const STABLECOIN_SWAP_ADDRESSES: { [chainId in ChainId]: string } = {
 
 export const BTC_SWAP_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0x4f6A43Ad7cba042606dECaCA730d4CE0A57ac62e",
-  [ChainId.HARDHAT]: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
+  [ChainId.HARDHAT]: "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
 }
 
 export const MERKLETREE_DATA: { [chainId in ChainId]: string } = {
@@ -75,7 +75,7 @@ export const BTC_SWAP_TOKEN_CONTRACT_ADDRESSES: {
   [chainId in ChainId]: string
 } = {
   [ChainId.MAINNET]: "0xC28DF698475dEC994BE00C9C9D8658A548e6304F",
-  [ChainId.HARDHAT]: "0x8dAF17A20c9DBA35f005b6324F493785D239719d",
+  [ChainId.HARDHAT]: "0x524F04724632eED237cbA3c37272e018b3A7967e",
 }
 
 export const BTC_SWAP_TOKEN = new Token(
@@ -99,7 +99,7 @@ export const STABLECOIN_SWAP_TOKEN = new Token(
 // Stablecoins
 const DAI_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-  [ChainId.HARDHAT]: "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
+  [ChainId.HARDHAT]: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
 }
 export const DAI = new Token(
   DAI_CONTRACT_ADDRESSES,
@@ -112,7 +112,7 @@ export const DAI = new Token(
 
 const USDC_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-  [ChainId.HARDHAT]: "0x9A676e781A523b5d0C0e43731313A708CB607508",
+  [ChainId.HARDHAT]: "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c",
 }
 export const USDC = new Token(
   USDC_CONTRACT_ADDRESSES,
@@ -125,7 +125,7 @@ export const USDC = new Token(
 
 const USDT_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-  [ChainId.HARDHAT]: "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
+  [ChainId.HARDHAT]: "0x59b670e9fA9D0A427751Af201D676719a970857b",
 }
 export const USDT = new Token(
   USDT_CONTRACT_ADDRESSES,
@@ -167,7 +167,7 @@ export const TBTC = new Token(
 
 const WBTC_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-  [ChainId.HARDHAT]: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+  [ChainId.HARDHAT]: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
 }
 export const WBTC = new Token(
   WBTC_CONTRACT_ADDRESSES,
@@ -180,7 +180,7 @@ export const WBTC = new Token(
 
 const RENBTC_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0xeb4c2781e4eba804ce9a9803c67d0893436bb27d",
-  [ChainId.HARDHAT]: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+  [ChainId.HARDHAT]: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
 }
 export const RENBTC = new Token(
   RENBTC_CONTRACT_ADDRESSES,
@@ -193,7 +193,7 @@ export const RENBTC = new Token(
 
 const SBTC_CONTRACT_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: "0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6",
-  [ChainId.HARDHAT]: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+  [ChainId.HARDHAT]: "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
 }
 export const SBTC = new Token(
   SBTC_CONTRACT_ADDRESSES,
