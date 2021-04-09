c8987742ed32bab437b5594630c140a463d99c67
David Hamme <dhamme@gmail.com>
2021-04-09 01:56:11 -0700

    Add jazzicon profile image

---
+++ src/components/Identicon.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement, useEffect, useRef } from "react"
+
+import Jazzicon from "@metamask/jazzicon"
+import styled from "@emotion/styled"
+import { useActiveWeb3React } from "../hooks"
+
+const StyledIdenticonContainer = styled.div`
+  height: 24px;
+  width: 24px;
+  border-radius: 1.125rem;
+  display: inline-block;
+  margin-left: 8px;
+  vertical-align: text-bottom;
+`
+
+export default function Identicon(): ReactElement {
+  const ref = useRef<HTMLDivElement>()
+
+  const { account } = useActiveWeb3React()
+
+  useEffect(() => {
+    if (account && ref.current) {
+      ref.current.innerHTML = ""
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      ref.current.appendChild(Jazzicon(24, parseInt(account.slice(2, 10), 16)))
+    }
+  }, [account])
+
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
+  return (
+    <StyledIdenticonContainer ref={ref as React.RefObject<HTMLDivElement>} />
+  )
+}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -21,3 +21,7 @@ interface Window {
   gtag?: (...args: any[]) => void
   web3?: unknown
 }
+
+declare module "@metamask/jazzicon" {
+  export default function (diameter: number, seed: number): HTMLElement
+}
