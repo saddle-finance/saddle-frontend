2cb219f2f2e068baab112fa22c36c408ad4b0cf2
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2021-04-16 16:55:20 -0700

    Use ethcall and Multicall to load pool token balances (#422)
    * Use ethcall and Multicall to load pool token balances
    
    Closes #391
    
    * Update multicallAddress
    
    * Remove unnecessary spread
    
    * Address comments
    
    Co-authored-by: Jongseung (John) Lim <penandlim@gmail.com>

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,2 +1,5 @@
 const PRODUCTION = "production"
 export const IS_PRODUCTION = process.env.NODE_ENV === PRODUCTION
+
+const DEVELOPMENT = "development"
+export const IS_DEVELOPMENT = process.env.NODE_ENV === DEVELOPMENT
