94040734b0c3f12265065b348b12eee515692d90
Waleed Malik <wmalik@nextdoor.com>
2021-08-07 19:46:56 -0400

    add assertions

--- cypress/integration/withdrawal.test.ts
+++ cypress/integration/withdrawal.test.ts
@@ -1,9 +1,16 @@
 import { PoolName } from "../../src/constants"
 
-const poolTokens: { [key: string]: string[] } = {
+// have two seperate maps here since the naming convention is different throughout the page
+const poolTokensFullName: { [key: string]: string[] } = {
   "BTC Pool": ["tBTC", "WBTC", "renBTC", "sBTC"],
   "Stablecoin Pool": ["Dai", "USDC Coin", "Tether"],
 }
+
+const poolTokens: { [key: string]: string[] } = {
+  "BTC Pool": ["TBTC", "WBTC", "RENBTC", "sBTC"],
+  "Stablecoin Pool": ["DAI", "USDC", "USDT"],
+}
+
 const pools = ["BTC Pool", "Stablecoin Pool"]
 
 context("Withdrawal Flow", () => {
@@ -44,15 +51,41 @@ context("Withdrawal Flow", () => {
           cy.get("button").contains("Withdraw").click()
         })
 
-      cy.wait(3000)
+      cy.wait(10000)
+
+      let beforeValue: { [key: string]: number } = {}
+      poolTokens[poolName].forEach((token: string) => {
+        cy.get(".myShareCard .tokenName")
+          .contains(token)
+          .parent()
+          .find("span.tokenValue")
+          .then(($value) => {
+            console.log($value)
+            beforeValue = {
+              ...beforeValue,
+              [token]: parseInt($value.text().replace(",", "")),
+            }
+          })
+      })
 
       // test single item
-      const tokens = poolTokens[poolName]
+
+      const tokens = poolTokensFullName[poolName]
       cy.get(".radio_wrapper .label").contains(tokens[0]).click()
       cy.get(".tokenInput input").first().type("1")
       cy.wait(500)
       cy.get("button").contains("Withdraw").click()
       cy.get("button").contains("Confirm Withdraw").click()
+      cy.wait(10000).then(() => {
+        cy.get(".myShareCard .tokenName")
+          .contains(poolTokens[poolName][0])
+          .parent()
+          .find("span.tokenValue")
+          .then(($value) => {
+            const afterValue = parseFloat($value.text().replace(",", ""))
+            expect(afterValue).to.lessThan(beforeValue[poolTokens[poolName][0]])
+          })
+      })
 
       // test combo withdraw through percentage option
       cy.get(".radio_wrapper .label").contains("Combo").click()
@@ -60,6 +93,18 @@ context("Withdrawal Flow", () => {
       cy.wait(500)
       cy.get("button").contains("Withdraw").click()
       cy.get("button").contains("Confirm Withdraw").click()
+      cy.wait(10000).then(() => {
+        poolTokens[poolName].forEach((token: string) => {
+          cy.get(".myShareCard .tokenName")
+            .contains(token)
+            .parent()
+            .find("span.tokenValue")
+            .then(($value) => {
+              const afterValue = parseFloat($value.text().replace(",", ""))
+              expect(afterValue).to.lessThan(beforeValue[token])
+            })
+        })
+      })
 
       // test combo withdraw by inputting values
       cy.get(".radio_wrapper .label").contains("Combo").click()
@@ -71,6 +116,18 @@ context("Withdrawal Flow", () => {
         cy.wait(500)
         cy.get("button").contains("Withdraw").click()
         cy.get("button").contains("Confirm Withdraw").click()
+      })
+      cy.wait(10000).then(() => {
+        poolTokens[poolName].forEach((token: string) => {
+          cy.get(".myShareCard .tokenName")
+            .contains(token)
+            .parent()
+            .find("span.tokenValue")
+            .then(($value) => {
+              const afterValue = parseFloat($value.text().replace(",", ""))
+              expect(afterValue).to.lessThan(beforeValue[token])
+            })
+        })
       })
     })
   }
