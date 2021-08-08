1150dd05c8f6f733268e20d9115a1e5161437fc6
Waleed Malik <wmalik@nextdoor.com>
2021-08-07 22:53:45 -0400

    adding unit tests for ethercanlink and shorten address

---
+++ src/utils/__tests__/shortenAddress.ts
@@ -0,0 +1,19 @@
+import { shortenAddress } from "../shortenAddress"
+
+describe("shortenAddress", () => {
+  it("catches an error for a bad address", () => {
+    expect(() => shortenAddress("yo what up waleed")).toThrow(Error)
+  })
+
+  it("shortens address", () => {
+    expect(shortenAddress("0xcd7587dd215ca6002c51f48c082af2d3fc77a6ca")).toBe(
+      "0xcD75...A6cA",
+    )
+  })
+
+  it("shortens address", () => {
+    expect(
+      shortenAddress("0xcd7587dd215ca6002c51f48c082af2d3fc77a6ca", 5),
+    ).toBe("0xcD758...7A6cA")
+  })
+})
