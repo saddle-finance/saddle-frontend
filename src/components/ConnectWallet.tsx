9ef4e12bd6fe53914f025a74a8a8be70815f306e
Jongseung Lim <penandlim@gmail.com>
2021-09-13 16:58:55 -0400

    Remove unnecessary console prints

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -25,7 +25,6 @@ function ConnectWallet({ onClose }: Props): ReactElement {
             onClick={(): void => {
               activate(wallet.connector, undefined, true).catch((error) => {
                 if (error instanceof UnsupportedChainIdError) {
-                  console.error(error)
                   void activate(wallet.connector) // a little janky...can't use setError because the connector isn't set
                 } else {
                   // TODO: handle error
