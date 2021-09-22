41d891055e99e2d12e4c8a7c01400a84103fcf69
ug02fast <arthurzhuk@gmail.com>
2021-09-22 13:38:55 -0700

    revert notify id, data-testid change

--- cypress/integration/risk.test.ts
+++ cypress/integration/risk.test.ts
@@ -5,9 +5,11 @@ context("Risk Flow", () => {
   })
 
   it("renders the Risk view and its contents", () => {
-    cy.get('[data-cy="risk-intro"]').contains("Providing liquidity to Saddle")
-    cy.get('[data-cy="risk-audits"]').contains("The Saddle smart")
-    cy.get('[data-cy="risk-adminkeys"]').contains("Saddle's admin keys")
-    cy.get('[data-cy="risk-lossofpeg"]').contains("If one of the assets")
+    cy.get('[data-testid="risk-intro"]').contains(
+      "Providing liquidity to Saddle",
+    )
+    cy.get('[data-testid="risk-audits"]').contains("The Saddle smart")
+    cy.get('[data-testid="risk-adminkeys"]').contains("Saddle's admin keys")
+    cy.get('[data-testid="risk-lossofpeg"]').contains("If one of the assets")
   })
 })
