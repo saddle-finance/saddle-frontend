ad6fbd5f41d85b4ded25e6663851185330f49d06
David Hamme <dhamme@gmail.com>
2021-10-21 13:11:09 +0100

    Add theme changer hook & provider (#679)
    Co-authored-by: Sunil Srivatsa <sunilsrivatsa@gmail.com>

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,36 +1,17 @@
 import "./ThemeChanger.scss"
 
-import { AppDispatch, AppState } from "../state"
-import React, { ReactElement, useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import React, { ReactElement, useContext } from "react"
 
-import { updateDarkMode } from "../state/user"
-import { useColorMode } from "@chakra-ui/react"
+import { ThemeContext } from "../providers/ThemeProvider"
 
 const ThemeChanger = (): ReactElement => {
-  const dispatch = useDispatch<AppDispatch>()
-  const { colorMode, toggleColorMode } = useColorMode()
-  const { userDarkMode } = useSelector((state: AppState) => state.user)
-
-  useEffect(() => {
-    if (userDarkMode) {
-      document.body.classList.add("dark")
-    } else {
-      document.body.classList.remove("dark")
-    }
-  }, [userDarkMode])
+  const { toggleTheme, userDarkMode } = useContext(ThemeContext)
 
   return (
     <div className="themeChanger">
       <button
         onClick={(): void => {
-          dispatch(updateDarkMode(!userDarkMode))
-          if (
-            (userDarkMode && colorMode === "dark") ||
-            (!userDarkMode && colorMode === "light")
-          ) {
-            toggleColorMode()
-          }
+          toggleTheme()
         }}
       >
         {userDarkMode ? "☾" : "☀"}
