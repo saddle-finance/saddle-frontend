76587d53eddeeb7f91cc7e90dbce211196128833
David Hamme <dhamme@gmail.com>
2021-11-12 12:29:14 -0800

    Site Settings Menu (#696)
    * i18n
    
    * working site settings menu
    
    * finish work on site settings including Ui and functionality

--- src/components/NetworkDisplay.tsx
+++ src/components/NetworkDisplay.tsx
@@ -8,14 +8,18 @@ import styles from "./NetworkDisplay.module.scss"
 import { useActiveWeb3React } from "../hooks"
 import { useTranslation } from "react-i18next"
 
-export default function NetworkDisplay(): ReactElement | null {
+export default function NetworkDisplay({
+  onClick,
+}: {
+  onClick: () => void
+}): ReactElement | null {
   const { chainId, active } = useActiveWeb3React()
   const { t } = useTranslation()
   const networkLabel: string =
     (chainId ? NETWORK_LABEL[chainId] : undefined) || t("unknown")
 
   return IS_L2_SUPPORTED ? (
-    <Button kind="ghost" size="medium">
+    <Button kind="ghost" size="medium" onClick={onClick}>
       <div
         className={classnames(styles.circle, { [styles.active]: active })}
       ></div>{" "}
