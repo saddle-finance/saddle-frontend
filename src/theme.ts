529f0d1848ba58cb862f8b862fb0e9ee47c5a38b
nkrulikovskiy <nkrulikovskiy@gmail.com>
2021-12-13 22:48:38 +0300

    Add dark and light theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,86 @@
 import { createTheme } from "@mui/material"
 
+// const seaweed = "#00F4D7"
+
 export const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#F00",
+      main: "#00f4d7",
     },
     secondary: {
-      main: "#f50057",
+      main: "#3800d6",
     },
     background: {
-      default: "#faf3ce",
+      default: "#ffffe9",
+    },
+  },
+  spacing: 8,
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        color: "inherit",
+      },
+      styleOverrides: {
+        colorInherit: {
+          backgroundColor: "#FFFFE9",
+          color: "#141414",
+          border: "1px solid #FFFFE9",
+          boxShadow: "transparent",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          border: "1px solid #000000",
+        },
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+
+    MuiTooltip: {
+      defaultProps: {
+        arrow: true,
+      },
+    },
+  },
+})
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#00f4d7",
+    },
+    secondary: {
+      main: "#3800d6",
+    },
+    background: {
+      default: "#000000",
+      paper: "rgba(0,0,0,0.7)",
+    },
+  },
+  spacing: 8,
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorInherit: {
+          backgroundColor: "#000",
+          color: "#fff",
+          border: "1px solid #000",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          border: "1px solid #FFF",
+        },
+      },
     },
   },
 })
