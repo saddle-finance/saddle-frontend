6ac1b996a6aa716671a1a75d1b62ab21fdafd9b6
ug02fast <arthurzhuk@gmail.com>
2021-12-14 12:39:09 -0800

    claim ui done, removed unused

--- src/pages/VestingClaim.tsx
+++ src/pages/VestingClaim.tsx
@@ -1,165 +1,190 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable sort-imports */
-import React, { ReactElement, useContext, useEffect, useState } from "react"
+import { Contract as EthersContract, ethers } from "ethers"
+import React, { ReactElement, useEffect, useState } from "react"
+import { SDL_TOKEN, SDL_TOKEN_ADDRESSES } from "../constants"
 import { Trans, useTranslation } from "react-i18next"
-import { commify, formatBNToString, getMulticallProvider } from "../utils"
-import { Contract } from "ethcall"
-import logo from "../assets/icons/logo.svg"
-import plusIcon from "../assets/icons/plus.svg"
+import { commify, formatBNToString } from "../utils"
+import { notifyCustomError, notifyHandler } from "../utils/notifyHandler"
 import Button from "../components/Button"
-import styles from "../components/TokenClaimModal.module.scss"
-import { SDL_TOKEN } from "../constants"
-import useAddTokenToMetamask from "../hooks/useAddTokenToMetamask"
 import INVESTOR_EMPLOYEE_VESTING_CONTRACT_ABI from "../constants/abis/vesting.json"
 import SDL_TOKEN_ABI from "../constants/abis/sdl.json"
-import { RewardsBalancesContext } from "../providers/RewardsBalancesProvider"
-import { useInvestorEmployeeVestingContract } from "../hooks/useContract"
-import { useActiveWeb3React } from "../hooks"
-import { vestingContractAddrs } from "../constants/vestingContractAddrs"
-import { MulticallContract } from "../types/ethcall"
-import { ethers, Contract as EthersContract } from "ethers"
+import { Sdl } from "../../types/ethers-contracts/Sdl"
+import TopMenu from "../components/TopMenu"
 import { Vesting } from "../../types/ethers-contracts/Vesting"
+import logo from "../assets/icons/logo.svg"
+import plusIcon from "../assets/icons/plus.svg"
+import styles from "../components/TokenClaimModal.module.scss"
+import { useActiveWeb3React } from "../hooks"
+import useAddTokenToMetamask from "../hooks/useAddTokenToMetamask"
 
 function VestingClaim(): ReactElement {
   const { t } = useTranslation()
   const { account, chainId, library } = useActiveWeb3React()
-  const investorEmployeeVestingContract = useInvestorEmployeeVestingContract()
-  const rewardBalances = useContext(RewardsBalancesContext)
   const { addToken, canAdd } = useAddTokenToMetamask({
     ...SDL_TOKEN,
     icon: `${window.location.origin}/logo.svg`,
   })
 
-  const formattedUnclaimedTokenbalance = commify(
-    formatBNToString(rewardBalances.total, 18, 0),
-  )
-
-  const isBeneficiaryAddress = false
-
-  const [vestingContracts, setVestingContracts] = useState({})
-  const [beneficiaries, setBeneficiaries] = useState([])
+  const [isValidBene, setIsValidBene] = useState(false)
+  const [claimableVestedAmount, setClaimableVestedAmount] = useState("0.0")
+  const [remainingAmount, setRemainingAmount] = useState("0.0")
+  const [vestingContract, setVestingContract] = useState<Vesting>()
 
   useEffect(() => {
-    const fetchVestingContracts = async () => {
-      const vestingContractsRes: Response = await fetch(
-        "https://gateway.pinata.cloud/ipfs/QmV73GEaijyiBFHu1vRdZBFffoCHaXYWG5SpurbEgr4VK6",
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        },
-      )
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const vestingContracts = await vestingContractsRes.json()
-      setVestingContracts(vestingContracts)
-    }
-    void fetchVestingContracts()
-    console.log({ account, vestingContracts, investorEmployeeVestingContract })
-  }, [])
-
-  useEffect(() => {
-    const fetchBeneficiaries = () => {
+    const fetchBeneficiaries = async () => {
       if (!library || !chainId) return
-      console.log({ library, chainId, window })
-      // const ethcallProvider = await getMulticallProvider(library, chainId)
-      const provider = new ethers.providers.Web3Provider(window.ethereum as any)
-      console.log({ provider })
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as never,
+      )
       const SDLContract = new EthersContract(
-        "0x04C89607413713Ec9775E14b954286519d836FEf",
+        SDL_TOKEN_ADDRESSES[chainId],
         SDL_TOKEN_ABI,
         provider.getSigner(),
+      ) as Sdl
+      const vestingContractDeployedFilter = SDLContract.filters.VestingContractDeployed(
+        null,
+        null,
       )
-      console.log({ SDLContract })
-      const filteredVestingContractDeployed = SDLContract.filters.VestingContractDeployed()
-      // const beneficiariesCalls = vestingContractAddrs.map((addr) => {
-      //   return new EthersContract(
-      //     addr,
-      //     INVESTOR_EMPLOYEE_VESTING_CONTRACT_ABI,
-      //     provider.getSigner(),
-      //   )
-      // ) as MulticallContract<Vesting>
-      // })
-      // .map((contract) => {
-      //   console.log({ contract, filters: contract.filters })
-      //   const eventFilter = contract.filters.beneficiary()
-      //   console.log({ eventFilter })
-      //   return contract.beneficiary()
-      // })
-      // console.log({
-      //   beneficiariesCalls,
-      // })
-      // const bRes = beneficiariesCalls[0].beneficiary()
-      // console.log({ bRes })
-      // const events = await beneficiariesCalls.queryFilter(eventFilter)
-      // const beneficiaries = await ethcallProvider.all(beneficiariesCalls, {})
-      // @ts-ignore
-      // setBeneficiaries(beneficiaries)
+      try {
+        const events = await SDLContract.queryFilter(
+          vestingContractDeployedFilter,
+        )
+        const currentBeneficiary = events.filter(
+          (event) => event.args?.beneficiary === account,
+        )
+        setIsValidBene(currentBeneficiary.length > 0)
+        if (isValidBene) {
+          const vestingContract = new EthersContract(
+            currentBeneficiary[0].args?.vestingContract,
+            INVESTOR_EMPLOYEE_VESTING_CONTRACT_ABI,
+            provider.getSigner(),
+          ) as Vesting
+          setVestingContract(vestingContract)
+          try {
+            const remainingAmount = commify(
+              formatBNToString(
+                await SDLContract.balanceOf(vestingContract.address),
+                18,
+                2,
+              ),
+            )
+            setRemainingAmount(remainingAmount)
+          } catch (err) {
+            console.error(err)
+            notifyCustomError({
+              ...(err as Error),
+              message: "Unable to get total pending amount",
+            })
+          }
+          try {
+            const claimableVestedAmount = commify(
+              formatBNToString(await vestingContract.vestedAmount(), 18, 2),
+            )
+            setClaimableVestedAmount(claimableVestedAmount)
+          } catch (err) {
+            console.error(err)
+            notifyCustomError({
+              ...(err as Error),
+              message: "Unable to get vested amount",
+            })
+          }
+        }
+      } catch (err) {
+        console.error(err)
+        notifyCustomError({
+          ...(err as Error),
+          message: "Unable to query for vesting contracts",
+        })
+      }
     }
     void fetchBeneficiaries()
-    console.log({ beneficiaries })
-  }, [])
+  }, [account, chainId, library, isValidBene])
+
+  const onClaimClick: () => void = async () => {
+    if (!vestingContract) return
+    try {
+      const txn = await vestingContract.release()
+      notifyHandler(txn?.hash, "claim")
+      await txn?.wait()
+      setClaimableVestedAmount("0.0")
+    } catch (err) {
+      console.error(err)
+      notifyCustomError({
+        ...(err as Error),
+        message: "Unable to claim vested tokens",
+      })
+    }
+  }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.gradient}></div>
-      <div className={styles.logoWrapper}>
-        <div className={styles.logo}>
-          <img src={logo} />
+    <>
+      <TopMenu activeTab={t("risk")} />
+      <div className={styles.container}>
+        <div className={styles.gradient}></div>
+        <div className={styles.logoWrapper}>
+          <div className={styles.logo}>
+            <img src={logo} />
+          </div>
         </div>
-      </div>
-      <div className={styles.mainContent}>
-        {isBeneficiaryAddress ? (
-          <div className={styles.tokenBalance}>
-            {formattedUnclaimedTokenbalance}
-            {canAdd && (
-              <img
-                src={plusIcon}
-                className={styles.plus}
-                onClick={() => addToken()}
-              />
-            )}
-          </div>
-        ) : (
-          <div className={styles.info}>
-            Please switch to your beneficiary address
-          </div>
-        )}
+        <div className={styles.mainContent}>
+          {isValidBene ? (
+            <>
+              <div className={styles.tokenBalance}>
+                {claimableVestedAmount} of {remainingAmount} remaining tokens
+                {canAdd && (
+                  <img
+                    src={plusIcon}
+                    className={styles.plus}
+                    onClick={() => addToken()}
+                  />
+                )}
+              </div>
+              <div className={styles.tokenBalanceHelpText}>
+                {t("totalClaimableSDL")}
+              </div>
+              <div className={styles.tokenBalanceHelpText}>
+                Vesting Contract:{" "}
+                <a
+                  href={`https://etherscan.io/address/${
+                    vestingContract?.address ?? ""
+                  }`}
+                >
+                  {vestingContract?.address}
+                </a>
+              </div>
+            </>
+          ) : (
+            <div className={styles.info}>
+              Please switch to your beneficiary address
+            </div>
+          )}
 
-        <div className={styles.tokenBalanceHelpText}>
-          {t("totalClaimableSDL")}
+          <div className={styles.info}>
+            <span>
+              <Trans i18nKey="saddleTokenInfo" t={t}>
+                SDL token is launched by Saddle Finance. Read more about token
+                distribution{" "}
+                <a
+                  href="https://blog.saddle.finance/introducing-sdl"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ textDecoration: "underline" }}
+                >
+                  here
+                </a>
+              </Trans>
+            </span>
+          </div>
+          {
+            <Button
+              onClick={onClaimClick}
+              disabled={!isValidBene || Number(claimableVestedAmount) === 0}
+            >
+              {t("claim")}
+            </Button>
+          }
         </div>
-        <div className={styles.info}>
-          <span>
-            <Trans i18nKey="saddleTokenInfo" t={t}>
-              SDL token is launched by Saddle Finance. Read more about token
-              distribution{" "}
-              <a
-                href="https://blog.saddle.finance/introducing-sdl"
-                target="_blank"
-                rel="noreferrer"
-                style={{ textDecoration: "underline" }}
-              >
-                here
-              </a>
-            </Trans>
-          </span>
-        </div>
-        {
-          <Button
-            onClick={() => investorEmployeeVestingContract?.release()}
-            disabled={!isBeneficiaryAddress}
-          >
-            {t("claim")}
-          </Button>
-        }
       </div>
-    </div>
+    </>
   )
 }
 
