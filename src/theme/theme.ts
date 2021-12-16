1ba3c0290d851784d48ec03568e37de1d608c471
nkrulikovskiy <nkrulikovskiy@gmail.com>
2021-12-16 03:13:30 +0300

    update theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,30 @@
 import { createTheme } from "@mui/material"
 
-// const seaweed = "#00F4D7"
+const saddleColors = {
+  indigo: "#4B11F2",
+  seafoam: "#06D7D7",
+  seaweed: "#00F4D7",
+  sunburn: "#E6AD76",
+  gold: "#FAEA5D",
+  white: "#FFF",
+  black: "#000",
+  grey70: "#252525",
+  grey50: "#404040",
+  grey25: "#7D7D7D",
+  grey5: "#A7A7A7",
+  grey1: "#E4E4E4",
+  indigo2: "#311188",
+  indigo3: "#121334",
+  indigo4: "#070713",
+  seafoam2: "#126969",
+  sunset: "#D96A3B",
+  sand1: "#E3D899",
+  sand2: "#FAF3CE",
+  sand3: "#FDFDF8",
+  gold2: "#817F48",
+  cherry: "#FB5A5A",
+  cherry2: "#68282F",
+}
 
 export const lightTheme = createTheme({
   palette: {
@@ -9,7 +33,7 @@ export const lightTheme = createTheme({
       main: "#3800d6",
     },
     secondary: {
-      main: "#00f4d7",
+      main: saddleColors.seaweed,
     },
     background: {
       default: "#ffffe9",
