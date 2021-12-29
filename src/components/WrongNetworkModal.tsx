09179d803886eb47b6771112b5af3e087ef9c5a6
nkrulikovskiy <nkrulikovskiy@gmail.com>
2021-12-29 12:05:01 +0200

    Add translation

--- src/components/WrongNetworkModal.tsx
+++ src/components/WrongNetworkModal.tsx
@@ -2,6 +2,7 @@ import { Box, Dialog, Typography } from "@mui/material"
 import React, { ReactElement } from "react"
 import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core"
 import { Web3Provider } from "@ethersproject/providers"
+import { useTranslation } from "react-i18next"
 
 interface Props {
   open?: boolean
@@ -10,6 +11,7 @@ interface Props {
 export default function WrongNetworkModal({ open }: Props): ReactElement {
   const { error } = useWeb3React<Web3Provider>()
   const isUnsupportChainIdError = error instanceof UnsupportedChainIdError
+  const { t } = useTranslation()
 
   return (
     <Dialog open={open ?? isUnsupportChainIdError} maxWidth="xs">
@@ -17,12 +19,7 @@ export default function WrongNetworkModal({ open }: Props): ReactElement {
         <Typography variant="h3" textAlign="center" mb={3}>
           &#129325;
         </Typography>
-        <p>
-          Opps, looks like you&apos;re trying to use Saddle while your wallet is
-          connected to an unsupported network.
-        </p>
-        <br />
-        <p>Please switch to Ethereum mainnet or another supported network.</p>
+        {t("wrongNetworkContent")}
       </Box>
     </Dialog>
   )
