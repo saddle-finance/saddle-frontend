b3809fc56d2a82c9133167440565b7702f77a171
Arthur <arthurzhuk@gmail.com>
2022-01-04 12:46:15 -0800

    no fail on uncaught

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -20,3 +20,9 @@ import "@cypress/code-coverage/support"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
