3e282f3fa943b53bab203dbbe64c904122817ac8
Karthik-Gupta <karthikts.gupta@gmail.com>
2022-01-09 12:40:42 +0530

    Revert - Test matic contracts on arbitrum config

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -2,8 +2,8 @@ import { ChainId } from "./index"
 
 export const NETWORK_LABEL: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: "Ethereum",
-  [ChainId.ARBITRUM]: "MaticMumbai",
-  //[ChainId.MATICMUMBAI]: "MaticMumbai",
+  [ChainId.ARBITRUM]: "Arbitrum",
+  [ChainId.MATICMUMBAI]: "MaticMumbai",
   [ChainId.ROPSTEN]: "Ropsten",
   [ChainId.HARDHAT]: "Hardhat 👷🏼‍♂️",
 }
