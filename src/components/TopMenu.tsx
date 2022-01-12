e23cbabbf2adbb2f0baab0acd3dff01b4cd73733
Nikita <75422800+nkrulikovsky@users.noreply.github.com>
2022-01-12 23:51:37 +0200

    Update token reward modal (#789)
    * add close button
    
    * test update
    
    * remove dark mode test
    
    * Change the component name
    
    * small fix
    
    * small css fix

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -6,12 +6,11 @@ import React, { ReactElement, useContext, useRef, useState } from "react"
 
 import Button from "./Button"
 import { IS_SDL_LIVE } from "../constants"
-import Modal from "./Modal"
 import NetworkDisplay from "./NetworkDisplay"
 import { RewardsBalancesContext } from "../providers/RewardsBalancesProvider"
 import { ReactComponent as SaddleLogo } from "../assets/icons/logo.svg"
 import SiteSettingsMenu from "./SiteSettingsMenu"
-import TokenClaimModal from "./TokenClaimModal"
+import TokenClaimDialog from "./TokenClaimDialog"
 import Web3Status from "./Web3Status"
 import classNames from "classnames"
 import { formatBNToShortString } from "../utils"
@@ -75,12 +74,10 @@ function TopMenu(): ReactElement {
           <NetworkDisplayAndSettings />
           <IconButtonAndSettings />
         </div>
-        <Modal
-          isOpen={!!currentModal}
+        <TokenClaimDialog
+          open={currentModal === "tokenClaim"}
           onClose={(): void => setCurrentModal(null)}
-        >
-          {currentModal === "tokenClaim" && <TokenClaimModal />}
-        </Modal>
+        />
       </Toolbar>
     </AppBar>
   )
