edca06c7a45fa17906c6d8fa1b8a38403f99e53e
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-01-14 18:01:28 +0200

    Name change and insert translation for copy

--- src/components/Copy.tsx
+++ src/components/Copy.tsx
@@ -3,19 +3,21 @@ import { Button } from "@mui/material"
 import CheckIcon from "@mui/icons-material/Check"
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 import useCopyClipboard from "../hooks/useCopyClipboard"
+import { useTranslation } from "react-i18next"
 
 export default function CopyHelper(props: {
   toCopy: string
   children?: React.ReactNode
 }): ReactElement {
   const [isCopied, setCopied] = useCopyClipboard()
+  const { t } = useTranslation()
 
   return (
     <Button
       onClick={() => setCopied(props.toCopy)}
       startIcon={isCopied ? <CheckIcon /> : <ContentCopyIcon />}
     >
-      {isCopied ? "Copied!" : props.children}
+      {isCopied ? t("copied") : t("copyAddress")}
     </Button>
   )
 }
