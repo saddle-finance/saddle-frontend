4700044c5b262111645d443538e113fff42e0387
Arthur Zhuk <arthurzhuk@gmail.com>
2022-01-19 12:02:45 -0800

    Optimism Launch (#783)
    * initial placement
    
    * using abbrv
    
    * addresses added, optimism added to metamask
    
    * addresses fixed
    
    * SDL address updated
    
    * Add token addresses on optimism network
    
    * added to isMetaPool
    
    * fixed bug for initializing token price
    
    Co-authored-by: Jongseung Lim <penandlim@gmail.com>

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -3,6 +3,7 @@ import { ChainId } from "./index"
 export const NETWORK_LABEL: Partial<Record<ChainId, string>> = {
   [ChainId.MAINNET]: "Ethereum",
   [ChainId.ARBITRUM]: "Arbitrum",
+  [ChainId.OPTIMISM]: "Optimism",
   [ChainId.ROPSTEN]: "Ropsten",
   [ChainId.HARDHAT]: "Hardhat 👷🏼‍♂️",
 }
