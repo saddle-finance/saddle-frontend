fb699e8c312c195e701a8f39329c7b81d025f79a
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-01-22 21:23:05 +0200

    disable ens on non-mainnet

--- src/hooks/useENS.ts
+++ src/hooks/useENS.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { ChainId } from "../constants"
 import { ethers } from "ethers"
 import { useActiveWeb3React } from "."
 
@@ -6,18 +7,23 @@ type ReturnType = { ensName: string | null }
 
 export const useENS = (address: string | null | undefined): ReturnType => {
   const [ensName, setENSName] = useState<string | null>(null)
-  const { library } = useActiveWeb3React()
+  const { library, chainId } = useActiveWeb3React()
 
   useEffect(() => {
     async function resolveENS() {
-      if (address && library && ethers.utils.isAddress(address)) {
+      if (
+        address &&
+        library &&
+        ethers.utils.isAddress(address) &&
+        chainId === ChainId.MAINNET
+      ) {
         const provider = new ethers.providers.Web3Provider(library.provider)
         const ensName = await provider.lookupAddress(address)
         if (ensName) setENSName(ensName)
       }
     }
     resolveENS().catch(console.error)
-  }, [address, library])
+  }, [address, library, chainId])
 
   return { ensName }
 }
