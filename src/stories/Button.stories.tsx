57b6457ebb38d074640b43c8485e7498acef6c3d
Nikita <75422800+nkrulikovsky@users.noreply.github.com>
2022-01-28 13:38:57 +0200

    Update Button variant (#801)
    * palette update
    
    * button hover update
    
    * Finish button variant
    
    * Add storybook
    
    * Integrate storybook
    
    * Integrate storybook
    
    * available only on light mode
    
    * remove storybook static

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { Button } from "@mui/material"
+import React from "react"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// Check controls from https://storybook.js.org/docs/react/essentials/controls
+
+export default {
+  title: "Light components/Button",
+  component: Button,
+  argTypes: {
+    variant: {
+      options: ["contained", "outlined", "text"],
+      control: { type: "inline-radio" },
+    },
+    color: {
+      options: ["primary", "secondary", "info", "mute", "error"],
+      control: { type: "select" },
+    },
+    disabled: {
+      options: [true, false],
+      control: { type: "inline-radio" },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "inline-radio" },
+    },
+  },
+} as ComponentMeta<typeof Button>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const LightButton = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+LightButton.args = {
+  variant: "contained",
+  children: "Button",
+  color: "secondary",
+}
--- src/theme/components/ButtonTheme.ts
+++ src/theme/components/ButtonTheme.ts
@@ -1,25 +1,81 @@
-import { Components, Theme } from "@mui/material"
+import {
+  ButtonPropsColorOverrides,
+  Components,
+  ComponentsVariants,
+  Theme,
+} from "@mui/material"
+import { OverridableStringUnion } from "@mui/types"
 
 declare module "@mui/material/Button" {
   interface ButtonPropsColorOverrides {
     secondaryLight: true
   }
 }
+type ColorVariant = OverridableStringUnion<
+  "primary" | "secondary" | "success" | "error" | "info" | "warning",
+  ButtonPropsColorOverrides
+>
+
+const buttonColorVariants: ColorVariant[] = ["primary", "secondary", "info"]
+
+const containedStyle = (
+  colors: ColorVariant[],
+  theme: Theme,
+): ComponentsVariants["MuiButton"] =>
+  colors.map((color) => ({
+    props: {
+      variant: "contained",
+      color: color,
+    },
+    style: {
+      "&:hover": {
+        backgroundColor: theme.palette[color].states?.containedHoverBackground,
+      },
+    },
+  }))
+
+const outlinedStyle = (
+  colors: ColorVariant[],
+  theme: Theme,
+): ComponentsVariants["MuiButton"] =>
+  colors.map((color) => ({
+    props: {
+      variant: "outlined",
+      color: color,
+    },
+    style: {
+      border: `1px solid ${
+        theme.palette[color].states?.outlinedRestingBorder ||
+        theme.palette[color].main
+      }`,
+      "&:hover": {
+        backgroundColor: theme.palette[color].states?.outlinedHoverBackground,
+      },
+    },
+  }))
+
+const textStyle = (
+  colors: ColorVariant[],
+  theme: Theme,
+): ComponentsVariants["MuiButton"] =>
+  colors.map((color) => ({
+    props: {
+      variant: "text",
+      color: color,
+    },
+    style: {
+      "&:hover": {
+        backgroundColor: theme.palette[color].states?.outlinedHoverBackground,
+      },
+    },
+  }))
 export default function ButtonTheme(theme: Theme): Components {
   return {
     MuiButton: {
-      variants: [
-        {
-          props: {
-            variant: "contained",
-            size: "small",
-          },
-          style: {
-            minWidth: 0,
-            padding: 1,
-          },
-        },
-      ],
+      variants: outlinedStyle(buttonColorVariants, theme)?.concat(
+        containedStyle(buttonColorVariants, theme) ?? [],
+        textStyle(buttonColorVariants, theme) ?? [],
+      ),
       styleOverrides: {
         root: {
           textTransform: "none",
@@ -31,17 +87,16 @@ export default function ButtonTheme(theme: Theme): Components {
             boxShadow: "none",
           },
         },
-        outlined: {
-          "&:hover": {
-            backgroundColor: theme.palette.secondaryLight.main,
-          },
-        },
         sizeMedium: {
           lineHeight: 0,
           minWidth: 70,
-          maxHeight: 32,
+          height: 32,
           padding: 8,
           font: theme.typography.body1.font,
+        },
+        sizeSmall: {
+          minWidth: 0,
+          padding: 1,
         },
       },
     },
