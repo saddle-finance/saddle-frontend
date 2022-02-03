58be4b62e7d8f96b8e6330bd1b09ea7bacca6e2d
Arthur <arthurzhuk@gmail.com>
2022-02-03 12:08:48 -0400

    trying snackbarcontent wip

--- src/providers/SnackbarProvider.tsx
+++ src/providers/SnackbarProvider.tsx
@@ -1,7 +1,8 @@
-import { IconButton, Slide, Snackbar } from "@mui/material"
+import { IconButton, SnackbarContent, Stack } from "@mui/material"
 import React, { ReactElement, createContext, useContext, useState } from "react"
 
 import CloseIcon from "@mui/icons-material/Close"
+// import { getEtherscanLink } from "../utils/getEtherscanLink"
 
 type SnackbarContextProps = {
   addSnackbar: (snackbar: { msg: string; id: string; type: string }) => void
@@ -23,39 +24,54 @@ export const SnackbarContextProvider = (
   const [snackbars, setSnackbars] = useState<
     { msg: string; id: string; type: string }[]
   >([])
-  const [open, setOpen] = useState(false)
+  // const [open, setOpen] = useState(false)
 
   const addSnackbar = (snackbar: { msg: string; id: string; type: string }) => {
     setSnackbars(
       (snackbars) =>
         [...snackbars, snackbar] as { msg: string; id: string; type: string }[],
     )
-    setOpen(true)
+    // setOpen(true)
+  }
+
+  const message = (msg: string, type: string) => {
+    console.log({ msg })
+    switch (type) {
+      case "deposit":
+        return `Deposit sent to network`
+      // return `Deposit ${msg} tx successful ${getEtherscanLink(msg, "tx")}`
+      default:
+        return msg
+    }
   }
 
   return (
     <SnackbarContext.Provider value={{ addSnackbar }}>
       {props.children}
-      {snackbars.map((snackbar) => (
-        <Snackbar
-          open={open}
-          anchorOrigin={{ vertical: "top", horizontal: "left" }}
-          autoHideDuration={9000}
-          message={snackbar.msg}
-          key={snackbar.id}
-          TransitionComponent={(props) => <Slide {...props} direction="down" />}
-          onClose={() => setOpen(false)}
-          action={
-            <IconButton
-              color="inherit"
-              size="small"
-              onClick={() => setOpen(false)}
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          }
-        />
-      ))}
+      <Stack spacing={2} sx={{ maxWidth: 600 }}>
+        {snackbars.map((snackbar) => (
+          <SnackbarContent
+            // open={open}
+            // anchorOrigin={{ vertical: "top", horizontal: "left" }}
+            // autoHideDuration={9000}
+            message={message(snackbar.msg, snackbar.type)}
+            key={snackbar.id}
+            // TransitionComponent={(props) => (
+            //   <Slide {...props} direction="down" />
+            // )}
+            // onClose={() => setOpen(false)}
+            action={
+              <IconButton
+                color="inherit"
+                size="small"
+                // onClick={() => setOpen(false)}
+              >
+                <CloseIcon fontSize="small" />
+              </IconButton>
+            }
+          />
+        ))}
+      </Stack>
     </SnackbarContext.Provider>
   )
 }
