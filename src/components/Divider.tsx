742f0c812c2677bebcfb9c81ca8bff82b848e67d
Nikita <75422800+nkrulikovsky@users.noreply.github.com>
2022-02-16 01:46:00 +0200

    Replace Divider with MUI (#833)

--- src/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from "react"
-
-import styles from "./Divider.module.scss"
-
-const Divider = (): ReactElement => <div className={styles.divider}></div>
-
-export default Divider
