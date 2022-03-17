12e131ce4ec1d75e83fb403f0a48bece22154d66
Arthur <arthurzhuk@gmail.com>
2022-03-17 16:49:41 -0700

    waiting

--- cypress/integration/withdrawal.test.ts
+++ cypress/integration/withdrawal.test.ts
@@ -47,11 +47,8 @@ context("Withdrawal Flow", () => {
       const tokens = poolTokensFullName[poolName]
       cy.get('[data-testid="withdrawTokenRadio"]').contains(tokens[0]).click()
       cy.get('[data-testid="myFarmLpBalance"]').should("not.have.text", "0.0")
-      cy.get("#tokenInput input")
-        .first()
-        .type("1")
-        .type("{backspace}")
-        .type("1")
+      cy.get("#tokenInput input").first().type("1")
+      cy.wait(500)
       cy.get('[data-testid="withdrawBtn"]').click()
       cy.get("[data-testid=tokenValue]")
         .first()
@@ -66,6 +63,7 @@ context("Withdrawal Flow", () => {
       // test combo withdraw through percentage option
       cy.get('[data-testid="withdrawPercentageCombo"]').click()
       cy.get('[data-testid="withdrawPercentageInput"]').type("3")
+      cy.wait(500)
       cy.get("button").contains("Withdraw").click()
       cy.get("[data-testid=tokenValue]")
         .first()
@@ -95,6 +93,7 @@ context("Withdrawal Flow", () => {
               "not.have.text",
               "0.0",
             )
+            cy.wait(500)
             cy.get("button").contains("Withdraw").click()
             cy.get("button").contains("Confirm Withdraw").click()
             cy.get("[data-testid=tokenValue]")
