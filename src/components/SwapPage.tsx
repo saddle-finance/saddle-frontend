d0ec8c615d82d610b09e9fc2b69122c340f02b24
Nikita <75422800+nkrulikovsky@users.noreply.github.com>
2022-03-18 21:35:25 +0300

    Storybook of SwapTokenInput (#831)
    * Textfield storybook
    
    * SwaptokenInput storybook
    
    * token input storybook
    
    * Textfield storybook
    
    * revamp
    
    * add testing data for swaptokenInput
    
    * add testing data for swaptokenInput
    
    * update Swap token input
    
    * update SwapTokenInput
    
    * remove console
    
    * update SwapTokenInput on swap page
    
    * add badge on SwaptokenInput
    
    * swaptokeninput testing
    
    * update SwapTokenInput on swap page
    
    * fix testing
    
    * SwapTokenInput testing fix
    
    * remove SearchInput and searchSelect
    
    * remove useDetectOutsideClick because we can use "ClickAwayListener" of MUI
    
    * update textfield theme
    
    * update textfield theme
    
    * update Textfield
    
    * Textfield update
    
    * add isswapfrom
    
    * remove unnecessary props
    
    * remove unnecessary wrapper
    
    * change swapform wrapper
    
    * give dynamic popover width
    
    * recreate package-lock.json
    
    * fix test
    
    * small fix
    
    * remove dark theme on the preview.js of storybook
    
    * fix test
    
    * fix typo
    
    * remove unnecessary props from TextField on Advancedoption.tsx
    
    * fix small issue
    
    * small fix
    
    * small fix
    
    * remove package-lock.json
    
    * testing
    
    * fix: testing issue

--- src/components/SwapPage.tsx
+++ src/components/SwapPage.tsx
@@ -1,6 +1,13 @@
 import "./SwapPage.scss"
 
-import { Box, Button, Container, Dialog } from "@mui/material"
+import {
+  Box,
+  Button,
+  Container,
+  Dialog,
+  Paper,
+  Typography,
+} from "@mui/material"
 import React, { ReactElement, useMemo, useState } from "react"
 import { SWAP_TYPES, getIsVirtualSwap } from "../constants"
 import { formatBNToPercentString, formatBNToString } from "../utils"
@@ -14,7 +21,7 @@ import { PendingSwap } from "../hooks/usePendingSwapData"
 import PendingSwapModal from "./PendingSwapModal"
 import ReviewSwap from "./ReviewSwap"
 import { Slippages } from "../state/user"
-import SwapInput from "./SwapInput"
+import SwapTokenInput from "./SwapTokenInput"
 import type { TokenOption } from "../pages/Swap"
 import { Zero } from "@ethersproject/constants"
 import classNames from "classnames"
@@ -101,31 +108,37 @@ const SwapPage = (props: Props): ReactElement => {
       parseFloat(slippageCustom?.valueRaw || "0") < 0.5)
 
   return (
-    <Container>
+    <Container maxWidth="sm" sx={{ mt: 5, mb: 20 }}>
       <div className="swapPage">
-        <div className="content">
-          <div className="swapForm">
-            <div className="row">
-              <h3 className="swapTitle">{t("from")}</h3>
-              <div className="balanceContainer">
-                <span>{t("balance")}:</span>
-                &nbsp;
-                <a
-                  onClick={() => {
-                    if (fromToken == null) return
-                    const amtStr = formatBNToString(
-                      fromToken.amount,
-                      fromToken.decimals || 0,
-                    )
-                    onChangeFromAmount(amtStr)
-                  }}
-                >
-                  {formattedBalance}
-                </a>
-              </div>
-            </div>
-            <div className="row">
-              <SwapInput
+        <Paper>
+          <Box p={{ xs: 3, md: 4 }} flex={1}>
+            <Box mb={5}>
+              <Box display="flex">
+                <Typography variant="subtitle1" component="span">
+                  {t("from").toLocaleUpperCase()}
+                </Typography>
+                <Box width="max-content" mr={0} ml="auto">
+                  <Typography variant="subtitle2" component="span">
+                    {t("balance")}:
+                  </Typography>
+                  &nbsp;
+                  <Button
+                    size="small"
+                    onClick={() => {
+                      if (fromToken == null) return
+                      const amtStr = formatBNToString(
+                        fromToken.amount,
+                        fromToken.decimals || 0,
+                      )
+                      onChangeFromAmount(amtStr)
+                    }}
+                  >
+                    {formattedBalance}
+                  </Button>
+                </Box>
+              </Box>
+              <SwapTokenInput
+                data-testid="swapTokenInputFrom"
                 tokens={tokenOptions.from.filter(
                   ({ symbol }) => symbol !== toState.symbol,
                 )}
@@ -136,30 +149,31 @@ const SwapPage = (props: Props): ReactElement => {
                 inputValueUSD={fromState.valueUSD}
                 isSwapFrom={true}
               />
-            </div>
-            <div style={{ height: "48px" }}></div>
-            <div className="row">
-              <h3 className="swapTitle">{t("to")}</h3>
-            </div>
-            <div className="row">
-              <SwapInput
-                tokens={tokenOptions.to.filter(
-                  ({ symbol }) => symbol !== fromState.symbol,
-                )}
-                onSelect={onChangeToToken}
-                selected={toState.symbol}
-                inputValue={toState.value}
-                inputValueUSD={toState.valueUSD}
-                isSwapFrom={false}
-              />
-            </div>
+            </Box>
+            <Typography variant="subtitle1">
+              {t("to").toLocaleUpperCase()}
+            </Typography>
+
+            <SwapTokenInput
+              data-testid="swapTokenInputTo"
+              tokens={tokenOptions.to.filter(
+                ({ symbol }) => symbol !== fromState.symbol,
+              )}
+              onSelect={onChangeToToken}
+              selected={toState.symbol}
+              inputValue={toState.value}
+              inputValueUSD={toState.valueUSD}
+              isSwapFrom={false}
+            />
             <div style={{ height: "24px" }}></div>
             {fromState.symbol && toState.symbol && (
-              <div className="row">
+              <Box display="flex" justifyContent="space-between">
                 <div>
-                  <span>{t("rate")}</span>
+                  <Typography component="span">{t("rate")}</Typography>
                   &nbsp;
-                  <span>{exchangeRateInfo.pair}</span>
+                  <Typography component="span">
+                    {exchangeRateInfo.pair}
+                  </Typography>
                   &nbsp;
                   <button
                     className="exchange"
@@ -184,18 +198,18 @@ const SwapPage = (props: Props): ReactElement => {
                   </button>
                 </div>
                 <span className="exchRate">{formattedExchangeRate}</span>
-              </div>
+              </Box>
             )}
-            <div className="row">
+            <Box display="flex" justifyContent="space-between">
               <span>{t("priceImpact")}</span>
               <span>{formattedPriceImpact}</span>
-            </div>
+            </Box>
             {formattedRoute && (
               <>
-                <div className="row">
+                <Box display="flex" justifyContent="space-between">
                   <span>{t("route")}</span>
                   <span>{formattedRoute}</span>
-                </div>
+                </Box>
                 {isVirtualSwap && (
                   <div className="row">
                     <span></span>
@@ -218,144 +232,142 @@ const SwapPage = (props: Props): ReactElement => {
                 )}
               </>
             )}
-          </div>
-          {account && isHighPriceImpact(exchangeRateInfo.priceImpact) ? (
-            <div className="exchangeWarning">
-              {t("highPriceImpact", {
-                rate: formattedPriceImpact,
-              })}
-            </div>
-          ) : null}
-          {isVirtualSwap && (
-            <div className="virtualSwapInfoBubble">
-              <InfoIcon />
-              {t("crossAssetSwapsUseVirtualSwaps")} {"<"}
-              <a
-                href="https://docs.saddle.finance/saddle-faq#what-is-virtual-swap"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {t("learnMore")}
-              </a>
-              {">"}
-            </div>
-          )}
-          <AdvancedOptions />
-          <div className="pendingSwaps">
-            {pendingSwaps.map((pendingSwap) => {
-              const formattedSynthBalance = commify(
-                formatUnits(
-                  pendingSwap.synthBalance,
-                  pendingSwap.synthTokenFrom.decimals,
-                ),
-              )
-              return (
-                <div
-                  className="pendingSwapItem"
-                  key={pendingSwap.itemId?.toString()}
-                  onClick={() => {
-                    setActivePendingSwap(pendingSwap.itemId)
-                    setCurrentModal("pendingSwap")
-                  }}
-                >
-                  <span className="swapDetails">
-                    {formattedSynthBalance} {pendingSwap.synthTokenFrom.symbol}{" "}
-                    {"->"} {pendingSwap.tokenTo.symbol}
-                  </span>
-                  <div className="swapTimeContainer">
-                    <svg
-                      width="11"
-                      height="11"
-                      viewBox="0 0 11 11"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M5.23467 1H5.5C7.98605 1 10 3.01525 10 5.49924C10 7.98311 7.98618 10 5.5 10C3.01388 10 1 7.98469 1 5.49924C1 4.30732 1.46423 3.22282 2.21973 2.41912L2.60641 2.78249C1.93974 3.49169 1.53066 4.4476 1.53066 5.49924C1.53066 7.69191 3.30721 9.46943 5.5 9.46943C7.69273 9.46943 9.46934 7.69046 9.46934 5.49924C9.46934 3.39724 7.83438 1.67581 5.76533 1.5393V2.96008H5.23467V1Z"
-                        fill="black"
-                        stroke="black"
-                        strokeWidth="0.3"
-                        strokeMiterlimit="10"
-                      />
-                      <path
-                        d="M5.76204 5.52774L5.76861 5.53328L5.77577 5.53804C5.82206 5.5688 5.85082 5.61957 5.84998 5.67802L5.84997 5.67802V5.68017C5.84997 5.77327 5.77431 5.85 5.67911 5.85C5.62153 5.85 5.56861 5.81994 5.53676 5.77321L5.53241 5.76682L5.52742 5.76091L4.26017 4.26001L5.76204 5.52774Z"
-                        fill="black"
-                        stroke="black"
-                        strokeWidth="0.3"
-                      />
-                    </svg>
-                    <span className="swapTime">
-                      {Math.ceil(pendingSwap.secondsRemaining / 60)} min.
-                    </span>
-                  </div>
-                </div>
-              )
+          </Box>
+        </Paper>
+        {account && isHighPriceImpact(exchangeRateInfo.priceImpact) ? (
+          <div className="exchangeWarning">
+            {t("highPriceImpact", {
+              rate: formattedPriceImpact,
             })}
           </div>
-          <Box mt={3} width="100%">
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              fullWidth
-              onClick={(): void => {
-                setCurrentModal("review")
-              }}
-              disabled={!!error || +toState.value <= 0}
+        ) : null}
+        {isVirtualSwap && (
+          <div className="virtualSwapInfoBubble">
+            <InfoIcon />
+            {t("crossAssetSwapsUseVirtualSwaps")} {"<"}
+            <a
+              href="https://docs.saddle.finance/saddle-faq#what-is-virtual-swap"
+              target="_blank"
+              rel="noreferrer"
             >
-              {t("swap")}
-            </Button>
-          </Box>
-
-          <div className={classNames({ showError: !!error }, "error")}>
-            {error}
+              {t("learnMore")}
+            </a>
+            {">"}
           </div>
-          <Dialog
-            open={!!currentModal}
-            onClose={(): void => setCurrentModal(null)}
-            scroll="body"
-            maxWidth="xs"
-            fullWidth
-          >
-            {currentModal === "review" ? (
-              <ReviewSwap
-                onClose={(): void => setCurrentModal(null)}
-                onConfirm={async (): Promise<void> => {
-                  setCurrentModal("confirm")
-                  logEvent("swap", {
-                    from: fromState.symbol,
-                    to: toState.symbol,
-                  })
-                  await onConfirmTransaction?.()
-                  setCurrentModal(null)
+        )}
+        <AdvancedOptions />
+        <div className="pendingSwaps">
+          {pendingSwaps.map((pendingSwap) => {
+            const formattedSynthBalance = commify(
+              formatUnits(
+                pendingSwap.synthBalance,
+                pendingSwap.synthTokenFrom.decimals,
+              ),
+            )
+            return (
+              <div
+                className="pendingSwapItem"
+                key={pendingSwap.itemId?.toString()}
+                onClick={() => {
+                  setActivePendingSwap(pendingSwap.itemId)
+                  setCurrentModal("pendingSwap")
                 }}
-                data={{
-                  from: fromState,
-                  to: toState,
-                  exchangeRateInfo,
-                  txnGasCost,
-                  swapType,
-                }}
-              />
-            ) : null}
-            {currentModal === "confirm" ? <ConfirmTransaction /> : null}
-            {currentModal === "pendingSwap" ? (
-              <PendingSwapModal
-                pendingSwap={
-                  pendingSwaps.find(
-                    (p) => p.itemId === activePendingSwap,
-                  ) as PendingSwap
-                }
-                onClose={() => {
-                  setCurrentModal(null)
-                  setActivePendingSwap(null)
-                }}
-              />
-            ) : null}
-          </Dialog>
+              >
+                <span className="swapDetails">
+                  {formattedSynthBalance} {pendingSwap.synthTokenFrom.symbol}{" "}
+                  {"->"} {pendingSwap.tokenTo.symbol}
+                </span>
+                <div className="swapTimeContainer">
+                  <svg
+                    width="11"
+                    height="11"
+                    viewBox="0 0 11 11"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M5.23467 1H5.5C7.98605 1 10 3.01525 10 5.49924C10 7.98311 7.98618 10 5.5 10C3.01388 10 1 7.98469 1 5.49924C1 4.30732 1.46423 3.22282 2.21973 2.41912L2.60641 2.78249C1.93974 3.49169 1.53066 4.4476 1.53066 5.49924C1.53066 7.69191 3.30721 9.46943 5.5 9.46943C7.69273 9.46943 9.46934 7.69046 9.46934 5.49924C9.46934 3.39724 7.83438 1.67581 5.76533 1.5393V2.96008H5.23467V1Z"
+                      fill="black"
+                      stroke="black"
+                      strokeWidth="0.3"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M5.76204 5.52774L5.76861 5.53328L5.77577 5.53804C5.82206 5.5688 5.85082 5.61957 5.84998 5.67802L5.84997 5.67802V5.68017C5.84997 5.77327 5.77431 5.85 5.67911 5.85C5.62153 5.85 5.56861 5.81994 5.53676 5.77321L5.53241 5.76682L5.52742 5.76091L4.26017 4.26001L5.76204 5.52774Z"
+                      fill="black"
+                      stroke="black"
+                      strokeWidth="0.3"
+                    />
+                  </svg>
+                  <span className="swapTime">
+                    {Math.ceil(pendingSwap.secondsRemaining / 60)} min.
+                  </span>
+                </div>
+              </div>
+            )
+          })}
         </div>
+        <Box mt={3} width="100%">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            onClick={(): void => {
+              setCurrentModal("review")
+            }}
+            disabled={!!error || +toState.value <= 0}
+          >
+            {t("swap")}
+          </Button>
+        </Box>
+
+        <div className={classNames({ showError: !!error }, "error")}>
+          {error}
+        </div>
+        <Dialog
+          open={!!currentModal}
+          onClose={(): void => setCurrentModal(null)}
+          scroll="body"
+        >
+          {currentModal === "review" ? (
+            <ReviewSwap
+              onClose={(): void => setCurrentModal(null)}
+              onConfirm={async (): Promise<void> => {
+                setCurrentModal("confirm")
+                logEvent("swap", {
+                  from: fromState.symbol,
+                  to: toState.symbol,
+                })
+                await onConfirmTransaction?.()
+                setCurrentModal(null)
+              }}
+              data={{
+                from: fromState,
+                to: toState,
+                exchangeRateInfo,
+                txnGasCost,
+                swapType,
+              }}
+            />
+          ) : null}
+          {currentModal === "confirm" ? <ConfirmTransaction /> : null}
+          {currentModal === "pendingSwap" ? (
+            <PendingSwapModal
+              pendingSwap={
+                pendingSwaps.find(
+                  (p) => p.itemId === activePendingSwap,
+                ) as PendingSwap
+              }
+              onClose={() => {
+                setCurrentModal(null)
+                setActivePendingSwap(null)
+              }}
+            />
+          ) : null}
+        </Dialog>
       </div>
     </Container>
   )
