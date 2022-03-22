db10a8aa381587ecab9023d5c98da5eba6aa392f
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-03-22 18:27:10 +0300

    remove old toast component

--- src/components/ToastsContainer.tsx
+++
@@ -1,9 +0,0 @@
-import "./ToastsContainer.scss"
-
-import React, { ReactElement } from "react"
-
-export default function ToastsContainer({
-  children,
-}: React.PropsWithChildren<unknown>): ReactElement {
-  return <div className={"toast-container"}>{children}</div>
-}
