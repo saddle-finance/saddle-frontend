bae85dacdefbf296093ac5e000bd44974bde9987
ug02fast <arthurzhuk@gmail.com>
2022-04-07 16:25:44 -0700

    removed unnecc logic

--- cypress/integration/deposit.test.ts
+++ cypress/integration/deposit.test.ts
@@ -1,9 +1,6 @@
 import { PoolName } from "../../src/constants"
 
-const poolTokens: { [key: string]: string[] } = {
-  "BTC V2": ["WBTC", "RENBTC", "sBTC"],
-  "Stablecoin V2": ["DAI", "USDC", "USDT"],
-}
+const pools = ["BTC V2", "Stablecoin V2"]
 
 context("Deposit Flow", () => {
   beforeEach(() => {
@@ -11,28 +8,14 @@ context("Deposit Flow", () => {
     cy.visit(`${host}#/pools`)
     cy.wait(3000)
   })
+
   function testPoolDeposit(poolName: PoolName) {
     it(`successfully completes a deposit of all ${poolName} assets`, () => {
-      let beforeValue: { [key: string]: number } = {}
       cy.contains(poolName)
         .parents("[data-testid=poolOverview]")
         .within(() => {
           cy.get("button").contains("Deposit").click()
         })
-      // attempt to wait for pool data to load
-      cy.get("input").first({ timeout: 10000 }).should("be.enabled")
-      // TODO: assert default state of the page
-      // Get before value of each token in My Share section
-      poolTokens[poolName].forEach((token: string) => {
-        cy.get("[data-testid=tokenName]")
-          .contains(token)
-          .parent()
-          .find("[data-testid=tokenValue]")
-          .then(($value) => {
-            beforeValue = { ...beforeValue, [token]: parseFloat($value.text()) }
-          })
-      })
-
       cy.get("#tokenInput input").then(($inputs) => {
         cy.wrap($inputs).each(($input) => {
           cy.wrap($input).type("1")
@@ -43,37 +26,15 @@ context("Deposit Flow", () => {
           .then(($value) => {
             prevVal = $value.text()
           })
-        // TODO: assert price impact changes
-        cy.wait(500)
-        // click "deposit" to trigger review modal
         cy.get("button").contains("Deposit").first().click()
-        // TODO: assert review data
-        // click "confirm" to initiate the actual transactions
         cy.get("button").contains("Confirm Deposit").click()
-        // Wait and assert after value of each token has been increased by 1
-        // poolTokens[poolName].forEach((token: string) => {
         cy.get(".Toastify").contains(`Deposit on ${poolName} complete`)
-        // cy.log("token", token)
-        // cy.get("[data-testid=tokenValue]")
-        //   .first()
-        //   .then(($value) => {
-        //     const prevVal = $value.text()
-        // cy.get("button").contains("Confirm Withdraw").click()
         cy.get("[data-testid=tokenValue]")
           .first()
           .should("not.have.text", prevVal)
-        // })
-        // cy.get("[data-testid=tokenName]")
-        //   .contains(token)
-        //   .parent()
-        //   .find("[data-testid=tokenValue]")
-        //   .then(($value) => {
-        //     const afterValue = parseFloat($value.text())
-        //     expect(afterValue).to.eq(beforeValue[token] + 1)
-        //   })
-        // })
       })
     })
   }
-  ;["BTC V2", "Stablecoin V2"].forEach(testPoolDeposit)
+
+  pools.forEach(testPoolDeposit)
 })
