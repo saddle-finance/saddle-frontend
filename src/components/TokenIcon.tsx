708ef505c4cac2469dc2a815bfa4a1df82006d97
Duy Nguyen <nguyenaiden@gmail.com>
2022-04-11 13:33:11 -0700

    Sign commit

--- src/components/TokenIcon.tsx
+++ src/components/TokenIcon.tsx
@@ -21,7 +21,7 @@ export default function TokenIcon({
 
 /** Update the img tag's src to the default "unknown" icon if svg file is not found
  *
- * @param event The event object of the onError attribute within the img tag.
+ * @param event The event object of the onError attribute within the img tag
  */
 function handleTokenIconImageError(
   event: React.SyntheticEvent<HTMLImageElement, Event>,
