1fa628ac44e50609143ab46c51561161f5da259d
David Hamme <dhamme@gmail.com>
2022-04-25 12:38:39 -0700

    improve advanced options test

--- cypress/integration/advancedOption.test.ts
+++ cypress/integration/advancedOption.test.ts
@@ -12,67 +12,65 @@ context("Advanced option test", () => {
   })
 
   function advancedOptionTest(poolName: PoolName) {
-    const host = Cypress.env("DAPP_HOST") as string
-    cy.visit(`${host}#/pools`)
-    cy.wait(3000)
-    cy.contains(poolName)
-      .parents("[data-testid=poolOverview]")
-      .within(() => {
-        cy.get("button").contains("Withdraw").click()
-      })
-    cy.wait(10000)
+    it("behaves as expected", () => {
+      cy.contains(poolName)
+        .parents("[data-testid=poolOverview]")
+        .within(() => {
+          cy.get("button").contains("Withdraw").click()
+        })
+      cy.wait(10000)
 
-    cy.get("[data-testid=advOptionContainer]")
-      .should("exist")
-      .should("be.visible")
-    cy.get("[data-testid=advTableContainer]").then(($tableContainer) => {
-      if ($tableContainer.is(":visible")) {
-        cy.log("container is visible")
-      } else {
-        cy.get("[data-testid=advOptionContainer]").click()
-      }
-    })
-    cy.get("[data-testid=advTableContainer]").then(($table) => {
-      const tableDisplay = $table.css("display")
-      const isTableVisible = tableDisplay !== "none"
+      cy.get("[data-testid=advOptionContainer]")
+        .should("exist")
+        .should("be.visible")
+      cy.get("[data-testid=advTableContainer]").then(($tableContainer) => {
+        if ($tableContainer.is(":visible")) {
+          cy.log("container is visible")
+        } else {
+          cy.get("[data-testid=advOptionContainer]").click()
+        }
+      })
+      cy.get("[data-testid=advTableContainer]").then(($table) => {
+        const tableDisplay = $table.css("display")
+        const isTableVisible = tableDisplay !== "none"
 
-      if (!isTableVisible) {
-        cy.get("[data-testid=advOptionTitle]").click()
-      }
+        if (!isTableVisible) {
+          cy.get("[data-testid=advOptionTitle]").click()
+        }
+      })
+      cy.get("[data-testid=infiniteApprovalContainer]")
+        .find("input")
+        .check({ force: true })
+      cy.get("[data-testid=maxSlippageInputGroup]")
+        .find("button")
+        .each(($button) => {
+          cy.wrap($button)
+            .click()
+            .then(($clickedButton) => {
+              const buttonClass = $clickedButton.attr("class")
+              expect(buttonClass).to.match(/selected/)
+            })
+        })
+      cy.get("[data-testid=maxSlippageInputGroup]")
+        .find("input")
+        .type("0.1")
+        .should("have.value", "0.1")
+      cy.get("[data-testid=txnDeadlineInputGroup]")
+        .find("button")
+        .each(($button) => {
+          cy.wrap($button)
+            .click()
+            .then(($clickedButton) => {
+              const buttonClass = $clickedButton.attr("class")
+              expect(buttonClass).to.match(/selected/)
+            })
+        })
+      cy.get("[data-testid=txnDeadlineInputGroup]")
+        .find("input")
+        .type("20")
+        .should("have.value", "20")
     })
-    cy.get("[data-testid=infiniteApprovalContainer]")
-      .find("input")
-      .check({ force: true })
-    cy.get("[data-testid=maxSlippageInputGroup]")
-      .find("button")
-      .each(($button) => {
-        cy.wrap($button)
-          .click()
-          .then(($clickedButton) => {
-            const buttonClass = $clickedButton.attr("class")
-            expect(buttonClass).to.match(/selected/)
-          })
-      })
-    cy.get("[data-testid=maxSlippageInputGroup]")
-      .find("input")
-      .type("0.1")
-      .should("have.value", "0.1")
-    cy.get("[data-testid=txnDeadlineInputGroup]")
-      .find("button")
-      .each(($button) => {
-        cy.wrap($button)
-          .click()
-          .then(($clickedButton) => {
-            const buttonClass = $clickedButton.attr("class")
-            expect(buttonClass).to.match(/selected/)
-          })
-      })
-    cy.get("[data-testid=txnDeadlineInputGroup]")
-      .find("input")
-      .type("20")
-      .should("have.value", "20")
   }
-  it("input slippage value", () => {
-    pools.forEach(advancedOptionTest)
-  })
+
+  pools.forEach(advancedOptionTest)
 })
