96b6a4e63a0b56963b84543ff969365b3cbff9e1
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-05-17 18:13:44 +0300

    Fix test

--- cypress/integration/veSdl.test.ts
+++ cypress/integration/veSdl.test.ts
@@ -2,8 +2,12 @@ context("veSDL test", () => {
   beforeEach(() => {
     const host = Cypress.env("DAPP_HOST") as string
     cy.visit(`${host}#/vesdl`)
+    cy.wait(3000)
   })
   it("veSDL test", () => {
+    cy.getBySelId("lockVeSdlBtn").should("be.disabled")
+    cy.getBySelId("unlockVeSdlBtn").should("be.disabled")
+
     cy.getBySelId("sdlTokenInput")
       .find("input")
       .should("exist")
@@ -13,7 +17,5 @@ context("veSDL test", () => {
       .find("input")
       .type("12/02/2022")
       .should("have.value", "12/02/2022")
-    cy.getBySelId("lockVeSdlBtn").should("be.disabled")
-    cy.getBySelId("unlockVeSdlBtn").should("be.disabled")
   })
 })
