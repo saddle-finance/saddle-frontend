9a182601b627e093190c41fed6dd420e354b6adf
David Hamme <dhamme@gmail.com>
2022-05-19 14:53:28 -0700

    correct deposit test pools

--- cypress/integration/deposit.test.ts
+++ cypress/integration/deposit.test.ts
@@ -1,6 +1,6 @@
-// const STABLECOIN_POOL_V2_NAME = "Stablecoin V2"
+const STABLECOIN_POOL_V2_NAME = "Stablecoin V2"
 const SUSD_METAPOOL_V3_NAME = "sUSD-USDv2_v3"
-// const pools = [STABLECOIN_POOL_V2_NAME, SUSD_METAPOOL_V3_NAME] // order is important basepool must have balance prior to metapool
+const pools = [STABLECOIN_POOL_V2_NAME, SUSD_METAPOOL_V3_NAME] // order is important basepool must have balance prior to metapool
 
 context("Deposit Flow", () => {
   beforeEach(() => {
@@ -37,5 +37,5 @@ context("Deposit Flow", () => {
     })
   }
 
-  ;[SUSD_METAPOOL_V3_NAME].forEach(testPoolDeposit)
+  pools.forEach(testPoolDeposit)
 })
