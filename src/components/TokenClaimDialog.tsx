b93fda29e2c04ce5f00ffe9796afc622b1e04219
Arthur <arthurzhuk@gmail.com>
2022-05-31 13:55:32 -0700

    test

--- src/components/TokenClaimDialog.tsx
+++ src/components/TokenClaimDialog.tsx
@@ -381,7 +381,7 @@ function useRewardClaims() {
         })
         updateClaimStatus(pool.poolName, STATUSES.SUCCESS)
       } catch (e) {
-        console.error(e, "ok")
+        console.error(e)
         updateClaimStatus(pool.poolName, STATUSES.ERROR)
         enqueueToast("error", "Unable to claim reward")
       }
