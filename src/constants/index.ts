4e0299f944f7bf90a64b5cfaf19e667e44744daa
Arthur <arthurzhuk@gmail.com>
2022-06-09 11:22:48 -0700

    flag flip test

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1847,7 +1847,7 @@ export const SYNTH_TRACKING_ID =
 export const IS_VIRTUAL_SWAP_ACTIVE = true
 export const IS_L2_SUPPORTED = true
 export const IS_SDL_LIVE = true
-export const IS_VESDL_LIVE = false
+export const IS_VESDL_LIVE = true
 // FLAGS END
 
 // Regex for readable decimal number
