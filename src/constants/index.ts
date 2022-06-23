efd1221e4ee9921cf785e1b85e0482348302ada0
nkrulikovskiy <nkrulikovskiy@gmail.com>
2022-06-23 23:49:26 +0300

    set IS_ VESDL_LIVE false

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1859,7 +1859,7 @@ export const SYNTH_TRACKING_ID =
 export const IS_VIRTUAL_SWAP_ACTIVE = true
 export const IS_L2_SUPPORTED = true
 export const IS_SDL_LIVE = true
-export const IS_VESDL_LIVE = true
+export const IS_VESDL_LIVE = false
 export const IS_POOL_REGISTRY_MIGRATION_LIVE = false
 // FLAGS END
 
