c6e52df050e20b4d36177166d5c9752ed5237fa7
David Hamme <dhamme@gmail.com>
2022-06-24 00:06:50 -0400

    Add SDL price

--- src/providers/AprsProvider.tsx
+++ src/providers/AprsProvider.tsx
@@ -5,15 +5,15 @@ import {
   TokensContext,
 } from "../providers/TokensProvider"
 import React, { ReactElement, useContext } from "react"
-import { getPriceDataForPool, shiftBNDecimals } from "../utils"
 
 import { AppState } from "../state"
 import { BasicPoolsContext } from "./../providers/BasicPoolsProvider"
 import { BigNumber } from "ethers"
 import { GaugeContext } from "../providers/GaugeProvider"
 import { GaugeReward } from "../utils/gauges"
-import { Zero } from "@ethersproject/constants"
 import { parseUnits } from "@ethersproject/units"
+import { shiftBNDecimals } from "../utils"
+import useGaugeTVL from "../hooks/useGaugeTVL"
 import { useSelector } from "react-redux"
 
 type MinAndMax = {
@@ -47,29 +47,16 @@ function useGaugeAprs() {
   const basicPools = useContext(BasicPoolsContext)
   const tokens = useContext(TokensContext)
   const { gauges } = useContext(GaugeContext)
+  const getGaugeTVL = useGaugeTVL()
   const { tokenPricesUSD } = useSelector((state: AppState) => state.application)
 
   if (!basicPools || !tokens || !tokenPricesUSD) return null
 
   return Object.values(gauges).reduce((acc, gauge) => {
-    const { rewards, workingSupply } = gauge
-    const basicPool = basicPools?.[gauge.poolAddress ?? ""]
-    let gaugeAssetPrice = Zero
-    if (basicPool) {
-      const { assetPrice } = getPriceDataForPool(
-        tokens,
-        basicPool,
-        tokenPricesUSD,
-      )
-      gaugeAssetPrice = assetPrice
-    }
-    const rewardAprs = buildRewards(
-      rewards,
-      tokens,
-      tokenPricesUSD,
-      workingSupply || Zero,
-      gaugeAssetPrice,
-    )
+    const gaugeTVL = getGaugeTVL(gauge.address)
+    const { rewards } = gauge
+
+    const rewardAprs = buildRewards(rewards, tokens, tokenPricesUSD, gaugeTVL)
     return {
       ...acc,
       [gauge.address]: rewardAprs,
@@ -84,13 +71,11 @@ function buildRewards(
   gaugeRewards: GaugeReward[],
   tokens: NonNullable<BasicTokens>,
   tokenPricesUSD: Partial<{ [symbol: string]: number }>,
-  poolSupply: BigNumber,
-  poolAssetPrice: BigNumber,
+  amountStakedUSD: BigNumber,
 ): GaugeApr[] {
   return gaugeRewards.map(({ tokenAddress, rate: rewardPerSecond }) => {
     const rewardToken = tokens[tokenAddress]
     const rewardPrice = tokenPricesUSD[rewardToken?.symbol || ""] || 0
-    const amountStakedUSD = poolSupply.mul(poolAssetPrice) // 1e18 * 1e18 = 1e36
 
     if (rewardPrice === 0 || amountStakedUSD.isZero()) {
       const maxRewardPerDay = rewardPerSecond.mul(BN_DAY_IN_SECONDS)
@@ -107,7 +92,7 @@ function buildRewards(
     const rewardPerYearUSD = rewardPerYear.mul(
       parseUnits(rewardPrice.toFixed(3), 3),
     ) // 1e18 * 1e3 = 1e21
-    const rewardApr = shiftBNDecimals(rewardPerYearUSD, 33).div(amountStakedUSD) // (1e21 * 1e33 = 1e54) / 1e36 = 1e18
+    const rewardApr = shiftBNDecimals(rewardPerYearUSD, 15).div(amountStakedUSD) // (1e21 * 1e15 = 1e36) / 1e18 = 1e18
 
     return {
       rewardToken,
