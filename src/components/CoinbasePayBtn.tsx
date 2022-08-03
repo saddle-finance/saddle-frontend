aa97f847f3f069bebbf1ac6e233fe469737447de
Nikita <75422800+nkrulikovsky@users.noreply.github.com>
2022-08-03 21:04:18 +0300

    Add link button (#1218)

--- src/components/CoinbasePayBtn.tsx
+++ src/components/CoinbasePayBtn.tsx
@@ -1,7 +1,8 @@
+import { Box, styled } from "@mui/material"
 import React, { useEffect, useRef, useState } from "react"
 import CnButtonImg from "../assets/button-cbPay-compact-addCrypto.png"
+import { generateOnRampURL } from "@coinbase/cbpay-js"
 import { initOnRamp } from "@coinbase/cbpay-js"
-import { styled } from "@mui/material"
 
 const CoinbasePayButton = styled("button")(() => ({
   maxHeight: 40,
@@ -16,6 +17,29 @@ const CoinbasePayButton = styled("button")(() => ({
     cursor: "not-allowed",
   },
 }))
+const CoinbasePayLinkButton = styled("a")(() => ({
+  maxHeight: 40,
+  display: "block",
+  background: 0,
+  border: 0,
+  padding: 0,
+  margin: 0,
+  cursor: "pointer",
+  "&:disabled": {
+    filter: "saturate(0)",
+    cursor: "not-allowed",
+  },
+}))
+
+const onRampURL = generateOnRampURL({
+  appId: "5662715-5b2c-4c76-8934-4842f3d66746",
+  destinationWallets: [
+    {
+      address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+      assets: ["ETH", "USDC"],
+    },
+  ],
+})
 const CoinbasePayBtn: React.FC = () => {
   const [isReady, setIsReady] = useState<boolean>(false)
   const cbInstance = useRef<ReturnType<typeof initOnRamp>>()
@@ -61,13 +85,18 @@ const CoinbasePayBtn: React.FC = () => {
     }
   }
   return (
-    <CoinbasePayButton
-      id="cbpay-button-container"
-      onClick={handleClick}
-      disabled={!isReady}
-    >
-      <img src={CnButtonImg} />
-    </CoinbasePayButton>
+    <Box>
+      <CoinbasePayButton
+        id="cbpay-button-container"
+        onClick={handleClick}
+        disabled={!isReady}
+      >
+        <img src={CnButtonImg} />
+      </CoinbasePayButton>
+      <CoinbasePayLinkButton href={onRampURL} target="_blank">
+        <img src={CnButtonImg} />
+      </CoinbasePayLinkButton>
+    </Box>
   )
 }
 
