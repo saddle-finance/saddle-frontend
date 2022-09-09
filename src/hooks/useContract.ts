598944ac9de9ab24abcd41e0f1291826d9a8c5ec
Duy Nguyen <nguyenaiden@gmail.com>
2022-09-09 12:25:09 -0700

    Remove useContract

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -75,7 +75,7 @@ import { useActiveWeb3React } from "./index"
 export const POOL_REGISTRY_NAME = formatBytes32String("PoolRegistry")
 export const GAUGE_FACTORY_NAME = formatBytes32String("GaugeFactory")
 // returns null on errors
-export function useContract(
+function useContract(
   address: string | undefined,
   ABI: ContractInterface,
   withSignerIfPossible = true,
