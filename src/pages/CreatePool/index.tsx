9c23531bc4f5697846d173c18313947c1c095c16
Arthur <arthurzhuk@gmail.com>
2022-11-02 19:30:01 -0700

    UX adjusted for dropdown base pools

--- src/pages/CreatePool/index.tsx
+++ src/pages/CreatePool/index.tsx
@@ -4,10 +4,14 @@ import {
   Button,
   Container,
   Divider,
+  FormControl,
   IconButton,
   InputAdornment,
+  InputLabel,
   Link,
+  MenuItem,
   Paper,
+  Select,
   Stack,
   TextField,
   ToggleButton,
@@ -16,13 +20,14 @@ import {
   useTheme,
 } from "@mui/material"
 import { ChainId, PoolTypes } from "../../constants"
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack"
 import CircularProgress from "@mui/material/CircularProgress"
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever"
 import ERC20_ABI from "../../constants/abis/erc20.json"
 import { Erc20 } from "../../../types/ethers-contracts/Erc20"
+import { ExpandedPoolsContext } from "../../providers/ExpandedPoolsProvider"
 import ReviewCreatePool from "./CreatePoolDialog"
 import { Link as RouteLink } from "react-router-dom"
 import { getContract } from "../../utils"
@@ -34,6 +39,7 @@ export enum PoolType {
   UsdMeta = "usdMetapool",
   BtcMeta = "btcMetapool",
   Base = "basepool",
+  Empty = "",
 }
 
 export type ValidationStatus =
@@ -48,6 +54,22 @@ export default function CreatePool(): React.ReactElement {
   const { t } = useTranslation()
   const theme = useTheme()
   const poolRegistry = usePoolRegistry()
+  const expandedPools = useContext(ExpandedPoolsContext)
+  const expandedPoolsRemapped = Object.values(expandedPools.data.byName)
+    .map((pool) => ({
+      basePoolAddress: pool.basePoolAddress,
+      poolName: pool.poolName,
+      isSaddleApproved: pool.isSaddleApproved,
+      address: pool.poolAddress,
+      isPaused: pool.isPaused,
+      isMigrated: pool.isMigrated,
+      tokensLength: pool.tokens.length,
+    }))
+    .filter((pool) => pool.tokensLength < 4)
+    .filter((pool) => pool.isSaddleApproved)
+    .filter((pool) => !pool.basePoolAddress)
+    .filter((pool) => !pool.isMigrated)
+    .filter((pool) => !pool.isPaused)
   const { account, library, chainId } = useActiveWeb3React()
 
   const [disableCreatePool, setDisableCreatePool] = useState<boolean>(true)
@@ -61,9 +83,10 @@ export default function CreatePool(): React.ReactElement {
   const [poolName, setPoolName] = useState<string>("")
   const [poolSymbol, setPoolSymbol] = useState<string>("")
   const [aParameter, setAParameter] = useState<string>("")
-  const [poolType, setPoolType] = useState<PoolType>(PoolType.UsdMeta)
+  const [poolType, setPoolType] = useState<PoolType>(PoolType.Empty)
   const [assetType, setAssetType] = useState<PoolTypes>(2)
   const [tokenInputs, setTokenInputs] = useState<string[]>([""])
+  const [selectedTokensLength, setSelectedTokensLength] = useState(0)
   const [tokenInfo, setTokenInfo] = useState<
     {
       name: string
@@ -96,9 +119,10 @@ export default function CreatePool(): React.ReactElement {
     setPoolName("")
     setPoolSymbol("")
     setAParameter("")
-    setPoolType(PoolType.UsdMeta)
+    setPoolType(PoolType.Empty)
     setAssetType(0)
     setFee("")
+    setSelectedTokensLength(0)
     setTokenInfo([
       {
         name: "",
@@ -148,6 +172,7 @@ export default function CreatePool(): React.ReactElement {
 
   const minFee = "0.01"
   const maxFee = "1"
+  const maxAmountOfTokens = 4
 
   const feeError =
     !isValidNumber(fee) ||
@@ -361,46 +386,75 @@ export default function CreatePool(): React.ReactElement {
                 <Typography mb={2}>
                   {t("createPoolTypeDescription3")}
                 </Typography>
-                <ToggleButtonGroup
-                  value={poolType}
-                  color="secondary"
-                  size="large"
-                  exclusive
-                  onChange={(event, value: PoolType) => {
-                    if (value) {
-                      setPoolType(value)
-                      if (value === PoolType.Base) {
-                        setTokenInputs([tokenInputs[0], ""])
+                <FormControl fullWidth>
+                  <InputLabel id="pool-type-label">Choose Pool Type</InputLabel>
+                  <Select
+                    label="Choose Pool Type"
+                    id="pool-select"
+                    onChange={(e) => {
+                      if (e.target.value === PoolType.Base) {
+                        setPoolType(e.target.value as PoolType)
+                        setTokenInputs(["", ""])
                       } else {
-                        setTokenInputs([tokenInputs[0]])
+                        const tokensLength =
+                          expandedPools.data.byAddress[e.target.value as string]
+                            ?.tokens.length ?? 0
+                        setPoolType(
+                          expandedPools.data.byAddress[e.target.value as string]
+                            ?.poolName as PoolType,
+                        )
+                        setSelectedTokensLength(tokensLength)
+                        setTokenInputs([
+                          ...(Array(maxAmountOfTokens - tokensLength).fill(
+                            "",
+                            0,
+                          ) as string[]),
+                        ])
+                        setMetapoolBasepoolAddr(e.target.value as string)
                       }
-                    }
-                  }}
-                  fullWidth
-                >
-                  <ToggleButton value={PoolType.UsdMeta} size="large">
-                    {t("usdMetapool")}
-                  </ToggleButton>
-                  {chainId &&
-                    [ChainId.MAINNET, ChainId.HARDHAT].includes(chainId) && (
-                      <ToggleButton value={PoolType.BtcMeta}>
-                        {t("btcMetapool")}
-                      </ToggleButton>
-                    )}
-                  <ToggleButton value={PoolType.Base}>
-                    {t("basepool")}
-                  </ToggleButton>
-                </ToggleButtonGroup>
+                    }}
+                  >
+                    <MenuItem value={PoolType.Base}>Base Pool</MenuItem>
+                    {expandedPoolsRemapped.map((pool, i) => (
+                      <MenuItem key={i} value={pool.address}>
+                        {pool.poolName}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                {/* <ToggleButtonGroup */}
+                {/*   value={poolType} */}
+                {/*   color="secondary" */}
+                {/*   size="large" */}
+                {/*   exclusive */}
+                {/*   onChange={(event, value: PoolType) => { */}
+                {/*     if (value) { */}
+                {/*       setPoolType(value) */}
+                {/*       if (value === PoolType.Base) { */}
+                {/*         setTokenInputs([tokenInputs[0], ""]) */}
+                {/*       } else { */}
+                {/*         setTokenInputs([tokenInputs[0]]) */}
+                {/*       } */}
+                {/*     } */}
+                {/*   }} */}
+                {/*   fullWidth */}
+                {/* > */}
+                {/*   <ToggleButton value={PoolType.UsdMeta} size="large"> */}
+                {/*     {t("usdMetapool")} */}
+                {/*   </ToggleButton> */}
+                {/*   {chainId && */}
+                {/*     [ChainId.MAINNET, ChainId.HARDHAT].includes(chainId) && ( */}
+                {/*       <ToggleButton value={PoolType.BtcMeta}> */}
+                {/*         {t("btcMetapool")} */}
+                {/*       </ToggleButton> */}
+                {/*     )} */}
+                {/*   <ToggleButton value={PoolType.Base}> */}
+                {/*     {t("basepool")} */}
+                {/*   </ToggleButton> */}
+                {/* </ToggleButtonGroup> */}
               </Box>
 
-              <Box
-                flex={1}
-                sx={{
-                  opacity: poolType === PoolType.Base ? 1 : 0.5,
-                  cursor:
-                    poolType === PoolType.Base ? "default" : "not-allowed",
-                }}
-              >
+              <Box flex={1}>
                 <Typography variant="subtitle1">
                   {t("chooseAssetType")}
                 </Typography>
@@ -417,7 +471,6 @@ export default function CreatePool(): React.ReactElement {
                     if (value !== null) setAssetType(value)
                   }}
                   size="large"
-                  disabled={poolType !== PoolType.Base}
                 >
                   <ToggleButton value={PoolTypes.USD} color="secondary">
                     USD
@@ -431,107 +484,120 @@ export default function CreatePool(): React.ReactElement {
               </Box>
             </Stack>
 
-            <Box>
-              <Typography variant="subtitle1">
-                {t("addTokenAddress")}
-              </Typography>
-              <Divider sx={{ mb: 2 }} />
-              <Stack
-                direction="row"
-                mb={1}
-                alignItems="center"
-                justifyContent="space-between"
-                flexWrap="wrap"
-              >
-                {tokenInputs.map((tokenInput, index) => (
-                  <Box
-                    key={`token-input-${index}`}
-                    flexBasis={`calc(50% - ${theme.spacing(1.5)})`}
+            {poolType !== PoolType.Empty ? (
+              <>
+                <Box>
+                  <Typography variant="subtitle1">
+                    {t("addTokenAddress")}
+                  </Typography>
+                  <Divider sx={{ mb: 2 }} />
+                  <Stack
+                    direction="row"
+                    mb={1}
+                    alignItems="center"
+                    justifyContent="space-between"
+                    flexWrap="wrap"
                   >
-                    <TextField
-                      autoComplete="off"
-                      value={tokenInputs[index]}
-                      label={`Token ${index}`}
-                      fullWidth
-                      color={tokenInfo[index]?.checkResult ?? "primary"}
-                      margin="normal"
-                      onChange={(e) => {
-                        tokenInputs[index] = e.target.value
-                        setTokenInputs([...tokenInputs])
-                        if (
-                          new Set(tokenInputs).size !== tokenInputs.length &&
-                          e.target.value
-                        ) {
-                          setTokenInputErrorMsg("Duplicate Tokens Not Allowed")
-                        } else {
-                          setTokenInputErrorMsg("")
-                        }
-                      }}
-                      onBlur={(e) => void onTokenInputBlur(e.target, index)}
-                      helperText={
-                        tokenInfo[index]?.name
-                          ? `${tokenInfo[index]?.name} (${
-                              tokenInfo[index]?.symbol
-                            }: ${String(tokenInfo[index]?.decimals)} decimals)`
-                          : " "
-                      }
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            {index > 1 && (
-                              <IconButton
-                                onClick={() =>
-                                  setTokenInputs((prev) =>
-                                    prev.filter(
-                                      (value, tokenIndex) =>
-                                        index !== tokenIndex,
-                                    ),
-                                  )
-                                }
-                              >
-                                {inputLoading[index] ? (
+                    {tokenInputs.map((tokenInput, index) => (
+                      <Box
+                        key={`token-input-${index}`}
+                        flexBasis={`calc(50% - ${theme.spacing(1.5)})`}
+                      >
+                        <TextField
+                          autoComplete="off"
+                          value={tokenInputs[index]}
+                          label={`Token ${index}`}
+                          fullWidth
+                          color={tokenInfo[index]?.checkResult ?? "primary"}
+                          margin="normal"
+                          onChange={(e) => {
+                            tokenInputs[index] = e.target.value
+                            setTokenInputs([...tokenInputs])
+                            if (
+                              new Set(tokenInputs).size !==
+                                tokenInputs.length &&
+                              e.target.value
+                            ) {
+                              setTokenInputErrorMsg(
+                                "Duplicate Tokens Not Allowed",
+                              )
+                            } else {
+                              setTokenInputErrorMsg("")
+                            }
+                          }}
+                          onBlur={(e) => void onTokenInputBlur(e.target, index)}
+                          helperText={
+                            tokenInfo[index]?.name
+                              ? `${tokenInfo[index]?.name} (${
+                                  tokenInfo[index]?.symbol
+                                }: ${String(
+                                  tokenInfo[index]?.decimals,
+                                )} decimals)`
+                              : " "
+                          }
+                          InputProps={{
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                {(poolType === PoolType.Base && index > 1) ||
+                                (poolType !== PoolType.Base && index > 0) ? (
+                                  <IconButton
+                                    onClick={() =>
+                                      setTokenInputs((prev) =>
+                                        prev.filter(
+                                          (value, tokenIndex) =>
+                                            index !== tokenIndex,
+                                        ),
+                                      )
+                                    }
+                                  >
+                                    {inputLoading[index] ? (
+                                      <CircularProgress size={20} />
+                                    ) : (
+                                      <DeleteForeverIcon />
+                                    )}
+                                  </IconButton>
+                                ) : null}
+                                {index <= 1 && inputLoading[index] && (
                                   <CircularProgress size={20} />
-                                ) : (
-                                  <DeleteForeverIcon />
                                 )}
-                              </IconButton>
-                            )}
-                            {index <= 1 && inputLoading[index] && (
-                              <CircularProgress size={20} />
-                            )}
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                  </Box>
-                ))}
-                {(poolType === PoolType.UsdMeta ||
-                  poolType === PoolType.BtcMeta) && (
-                  <Box flexBasis={`calc(50% - ${theme.spacing(1.5)})`}>
-                    <TextField
-                      value={metapoolBasepoolAddr}
-                      fullWidth
-                      disabled
-                      color="primary"
-                      margin="normal"
-                      helperText={t("basepoolAddress")}
-                    />
-                  </Box>
-                )}
-              </Stack>
-            </Box>
-            <Button
-              variant="outlined"
-              size="large"
-              onClick={handleAddToken}
-              disabled={
-                (poolType === PoolType.Base && tokenInputs.length > 3) ||
-                poolType !== PoolType.Base
-              }
-              sx={{ mb: 4 }}
-            >
-              {t("addToken")}
-            </Button>
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                      </Box>
+                    ))}
+                    {poolType !== PoolType.Base && (
+                      <Box flexBasis={`calc(50% - ${theme.spacing(1.5)})`}>
+                        <TextField
+                          value={metapoolBasepoolAddr}
+                          fullWidth
+                          disabled
+                          color="primary"
+                          margin="normal"
+                          helperText={t("basepoolAddress")}
+                        />
+                      </Box>
+                    )}
+                  </Stack>
+                </Box>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  onClick={handleAddToken}
+                  disabled={
+                    (poolType === PoolType.Base && tokenInputs.length > 3) ||
+                    (poolType !== PoolType.Base &&
+                      tokenInputs.length >=
+                        maxAmountOfTokens - selectedTokensLength)
+                  }
+                  sx={{ mb: 4 }}
+                >
+                  {t("addToken")}
+                </Button>
+              </>
+            ) : (
+              <Typography>Select Pool Type to add Tokens</Typography>
+            )}
             <Box display="flex" justifyContent="center" pb={1}>
               {tokenInputErrorMsg ? (
                 <Typography color="error">{tokenInputErrorMsg}</Typography>
