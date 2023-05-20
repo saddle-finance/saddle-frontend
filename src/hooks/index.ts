f91dbed9312c19ecebb84860920a983eee66b48d
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-05-20 20:58:17 +0300

    update useActiveWeb3React with wagmi hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,19 +1,18 @@
 import { injectedMetaMaskProvider, injectedTallyProvider } from "../connectors"
+import { useAccount, useChainId, useProvider, useSigner } from "wagmi"
 import { useEffect, useState } from "react"
 
-import { ChainId } from "../constants/networks"
-import { NetworkContextName } from "../constants"
-import { Web3Provider } from "@ethersproject/providers"
-import { Web3ReactContextInterface } from "@web3-react/core/dist/types"
 import { isMobile } from "react-device-detect"
 import { useWeb3React as useWeb3ReactCore } from "@web3-react/core"
 
-export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & {
-  chainId?: ChainId
-} {
-  const context = useWeb3ReactCore<Web3Provider>()
-  const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName)
-  return context.active ? context : contextNetwork
+export function useActiveWeb3React() {
+  const chainId = useChainId()
+  const { address: account } = useAccount()
+  const provider = useProvider()
+  const signer = useSigner()
+  const library = signer || provider
+
+  return { chainId, account, library }
 }
 
 export function useEagerConnect(): boolean {
